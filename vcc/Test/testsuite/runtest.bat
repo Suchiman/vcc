995ff7ad8ccc16935b6dad9a82d79220a04d31bb
MarkHillebrand <MarkHillebrand@localhost>
2009-07-05 20:52:52 +0000

    Revert a chunk of r23097, turn on /b:/smoke in runtest.bat instead.

--- vcc/Test/testsuite/runtest.bat
+++ vcc/Test/testsuite/runtest.bat
@@ -1,3 +1,3 @@
 echo "Refresh file to see test results" > ts.out
 start n ts.out
-..\..\Host\bin\debug\vcc.exe /s vcc2 done examples > ts.out
+..\..\Host\bin\debug\vcc.exe /b:/smoke /s vcc2 done examples > ts.out
