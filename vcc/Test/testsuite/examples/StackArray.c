a738036600d33dab16ee2c187d57d8e42ff94d92
MarkHillebrand <MarkHillebrand@localhost>
2009-09-03 11:27:34 +0000

    Polish StackArray.c

--- vcc/Test/testsuite/examples/StackArray.c
+++ vcc/Test/testsuite/examples/StackArray.c
@@ -1,155 +1,137 @@
 #include <stdlib.h>
 #include "vcc.h"
 
-#ifdef VERIFY
-#define bool _Bool
-#else
-#define bool int
-#endif
+#define CAPACITY (100)
 
-spec( ispure size_t Capacity()
-    ensures(result > 0 && result <= 100); )
-
-struct Stack{
+struct Stack {
     size_t topOfStack;
-    int array[100];
-
-    invariant (is_malloc_root(this) && is_array_emb(this->array,Capacity(),this) 
-                 && this->topOfStack>=0 && this->topOfStack<= Capacity())
+    int array[CAPACITY];
+    invariant(is_malloc_root(this) && topOfStack <= CAPACITY)
 };
 
 frameaxiom
-bool IsEmpty(struct Stack * S)
+bool IsEmpty(struct Stack *S)
     reads(S)
     requires(wrapped(S))
-    ensures(result == (S->topOfStack == 0))
+    returns(S->topOfStack == 0)
 {
     return S->topOfStack == 0;
 }
 
 ispure
-bool IsFull(struct Stack * S)
+bool IsFull(struct Stack *S)
     reads(S)
     requires(wrapped(S))
-    ensures(result == (S->topOfStack == Capacity()))
+    returns(S->topOfStack == CAPACITY)
 {
-    return S->topOfStack == Capacity();
+    return S->topOfStack == CAPACITY;
 }
 
 ispure
-size_t Length(struct Stack * S)
+size_t Length(struct Stack *S)
     reads(S)
     requires(wrapped(S))
-    ensures(result == S->topOfStack)
+    returns(S->topOfStack)
 {
     return S->topOfStack;
 }
 
-struct Stack * CreateStack()
-    ensures(result == NULL || wrapped(result))
-    ensures(result == NULL || is_fresh(result))
-    ensures(result == NULL || IsEmpty(result))
+struct Stack *CreateStack()
+    ensures(result == NULL ||
+	    wrapped(result) && is_fresh(result) && IsEmpty(result))
 {
-        struct Stack * S;
+    struct Stack *S;
 
-        assume(Capacity() == 100);
-
-    S = malloc(sizeof(struct Stack));
+    S = malloc(sizeof(*S));
     if (S != NULL) {
-      S->topOfStack = 0;
-      wrap(S);
+        S->topOfStack = 0;
+        wrap(S);
     }
     return S;
 }
 
-
-void DisposeStack(struct Stack * S)
+void DisposeStack(struct Stack *S)
     requires(wrapped(S))
     writes(S)
 {
     unwrap(S);
-        free(S);
+    free(S);
 }
-void MakeEmpty(struct Stack * S)
-    requires(wrapped(S))
-    ensures(wrapped(S))
+
+void MakeEmpty(struct Stack *S)
+    maintains(wrapped(S))
     ensures(IsEmpty(S))
     writes(S)
-
 {
     unwrap(S);
-        S->topOfStack = 0;
+    S->topOfStack = 0;
     wrap(S);
 }
 
-
-
-void Push(struct Stack * S, int X)
-    requires(wrapped(S))  
-    ensures(wrapped(S))
+void Push(struct Stack *S, int X)
+    maintains(wrapped(S))
     requires(!IsFull(S))
     ensures(!IsEmpty(S))
+    ensures(S->topOfStack==old(S->topOfStack) + 1)
     writes(S)
 {
     unwrap(S);
-        S->array [ S->topOfStack ] = X;
-        S->topOfStack = S->topOfStack+1;
-    wrap(S);        
-}
-
-
-int Top(struct Stack * S)
-    requires(!IsEmpty(S))
-    requires(wrapped(S))
-    reads(S)
-{
-    assert(inv(S));
-    return S->array[ S->topOfStack-1 ];
-}
-
-void Pop(struct Stack * S)
-    requires(wrapped(S))  
-    ensures(wrapped(S))
-    requires(!IsEmpty(S))
-    writes(S)
-    ensures(!IsFull(S))
-{   unwrap(S);
-        S->topOfStack = S->topOfStack-1;
+    S->array[S->topOfStack++] = X;
     wrap(S);
 }
 
-int test_main()
+int Top(struct Stack *S)
+    requires(!IsEmpty(S))
+    requires(wrapped(S))
+    returns(S->array[S->topOfStack-1])
 {
-    struct Stack * S;
-    size_t i; 
+    return S->array[S->topOfStack-1];
+}
+
+void Pop(struct Stack *S)
+    maintains(wrapped(S))
+    requires(!IsEmpty(S))
+    ensures(!IsFull(S))
+    ensures(S->topOfStack==old(S->topOfStack) - 1)
+    writes(S)
+{
+    unwrap(S);
+    S->topOfStack--;
+    wrap(S);
+}
+
+void test_main()
+{
+    struct Stack *S;
+    size_t i;
     int j;
 
     S = CreateStack();
-    assume(S != NULL);
-    assert(IsEmpty(S));
-    i = 0;
-    while(i < Capacity()) 
-        invariant(wrapped(S))
-//      invariant(i == Length(S))
-    {   assume(!IsFull(S));
+
+    if (S!=NULL) {
+        assert(IsEmpty(S));
+        i = 0;
+        while (i < CAPACITY)
+            invariant(wrapped(S))
+            invariant(i == Length(S))
+        {   
             Push(S, unchecked((int)i));
-        i = i + 1;
-    }
+            i++;
+        }
 
-    while(i>0) 
-        invariant(wrapped(S))
-        //invariant(Length(S)>0)
-    {   assume(!IsEmpty(S));
-        j = Top(S);
-        //assert(j<Capacity());
-        Pop(S);
-        i = i-1;
-    }
-    //assert(IsEmpty(S));
+        while (i > 0)
+            invariant(wrapped(S))
+            invariant(i == Length(S))
+        {   
+            j = Top(S);
+            Pop(S);
+            i--;
+        }
 
-    DisposeStack(S);
-    return 0;
+        DisposeStack(S);
+    }
 }
+
 /*`
 Verification of Stack#adm succeeded.
 Verification of IsEmpty succeeded.
