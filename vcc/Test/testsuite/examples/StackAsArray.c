d5b57b132667947e2038220de97809e11fa772b4
MarkHillebrand <MarkHillebrand@localhost>
2009-09-04 06:42:10 +0000

    Polish PageSet.c and StackAsArray.c

--- vcc/Test/testsuite/examples/StackAsArray.c
+++ vcc/Test/testsuite/examples/StackAsArray.c
@@ -3,255 +3,191 @@
 
 #include <vcc.h>
 
-#ifdef VERIFY
-#define bool _Bool
-#else
-#define bool int
-#endif
+spec(struct AbsStack {
+    size_t high_mark;
+    size_t capacity;
+    size_t entries[size_t];
 
-spec(
-struct AbsStack {
-	size_t high_mark;
-	size_t capacity;
-    
-    spec(size_t entries[size_t];)
-    invariant( this->high_mark <= this->capacity )
-    invariant( is_malloc_root(this) )
-};
-)
-
+    invariant(high_mark <= capacity)
+    invariant(is_malloc_root(this))
+};)
 
 struct Stack{
     size_t topOfStack;
     size_t capacity;
-
     size_t *elements;
+    spec(struct AbsStack ^abs;)
+    spec(obj_t elementsAsArray;)
 
-    spec( struct AbsStack ^abs; )
-    spec( obj_t elementsAsArray;)
-
-    invariant( this->capacity >= 0 )
-    invariant( elementsAsArray == as_array(elements, capacity ) )
-
-    invariant( keeps(abs, elementsAsArray) )
-
-    invariant( is_malloc_root(this) )
-    invariant( is_malloc_root(elementsAsArray) )
-
-    invariant( topOfStack>=0 && topOfStack <= capacity )
-    invariant( AbstractionRelation( this ) )
+    invariant(elementsAsArray == as_array(elements,capacity))
+    invariant(keeps(abs, elementsAsArray))
+    invariant(is_malloc_root(this))
+    invariant(is_malloc_root(elementsAsArray))
+    invariant(topOfStack <= capacity)
+    invariant(AbstractionRelation(this))
 };
 
 spec(
 ispure
-bool AbstractionRelation( struct Stack *stack ) 
-//	reads( stack, stack->abs, array_range(stack->elements, stack->capacity) )
-	reads( set_universe() )
-	returns ((stack->capacity == stack->abs->capacity) &&
-			 (stack->topOfStack == stack->abs->high_mark) &&
-             (forall( size_t idx ; { stack->abs->entries[idx] }
-					  (0 <= idx) && ( idx < stack->topOfStack ) 
-						  ==> (stack->elements[idx] == stack->abs->entries[idx] ) ) ) )
-	;
-)
+bool AbstractionRelation(struct Stack *stack)
+    reads(set_universe())
+    returns((stack->capacity == stack->abs->capacity) &&
+        stack->topOfStack == stack->abs->high_mark &&
+        forall(size_t idx; idx < stack->topOfStack;
+            stack->elements[idx] == stack->abs->entries[idx]));)
 
 
 frameaxiom
-bool IsEmpty(struct Stack * S)
+bool IsEmpty(struct Stack *S)
     reads(S)
     requires(wrapped(S))
-    ensures(result == (S->abs->high_mark == 0))
+    returns(S->abs->high_mark == 0)
 {
-	assert(inv(S));
-	return S->topOfStack == 0;
+    return S->topOfStack == 0;
 }
 
-ispure 
-bool IsFull(struct Stack * S)
+ispure
+bool IsFull(struct Stack *S)
     reads(S)
     requires(wrapped(S))
-    ensures(result == (S->abs->high_mark == S->abs->capacity))
+    returns(S->abs->high_mark == S->abs->capacity)
 {
-	assert(inv(S));
-	assert(inv(S->abs));
     return S->topOfStack == S->capacity;
 }
 
-size_t Length(struct Stack * S)
-    reads(S)
+size_t Length(struct Stack *S)
     requires(wrapped(S))
-    ensures(result == S->abs->high_mark)
+    returns(S->abs->high_mark)
 {
-	assert(inv(S));
-	return S->topOfStack;
+    return S->topOfStack;
 }
 
-struct Stack * CreateStack( unsigned int max_capacity )
-    requires( max_capacity > 0 )
-    ensures(result == NULL || wrapped(result))
-    ensures(result == NULL || old(nested(result)))
-    ensures(result == NULL || IsEmpty(result))
-    ensures(result == NULL || max_capacity == result->abs->capacity)
-    ensures(result == NULL || is_fresh(result))
-    writes(set_empty())
+struct Stack *CreateStack(unsigned max_capacity)
+    requires(max_capacity > 0)
+    ensures(result == NULL ||
+        wrapped(result) &&
+        is_fresh(result) &&
+        IsEmpty(result) &&
+        result->abs->capacity == max_capacity)
 {
-	struct Stack * S;
-	
-    S = (struct Stack*)0;
+    struct Stack *S;
 
-	S = malloc(sizeof(struct Stack));
-	if (S != NULL) {
+    S = malloc(sizeof(struct Stack));
+    if (S == NULL) return NULL;
+
     S->topOfStack = 0;
-    S->capacity   = max_capacity;
-    S->elements    = malloc(sizeof(size_t) * S->capacity );
-    if (S->elements != NULL) {
+    S->capacity = max_capacity;
+    S->elements = malloc(sizeof(size_t) * S->capacity);
+    if (S->elements == NULL) {
+        free(S);
+        return NULL;
+    }
 
-speconly(
-
-      S->elementsAsArray = as_array(S->elements, S->capacity);
-      S->abs = spec_malloc<struct AbsStack>();
-      S->abs->high_mark = S->topOfStack ; 
-      S->abs->capacity  = S->capacity;
-      
-      set_owns( S->abs, set_empty() );
-      assert( S->abs->high_mark == S->topOfStack );
-
-      wrap( S->elementsAsArray );
-      set_owner( S->elementsAsArray, S );
-        
-      wrap( S->abs );
-      set_owner( S->abs, S );
-)
-      wrap(S);
-    } 
-    else return NULL;
-	}
-  return S;
+    speconly(
+        S->elementsAsArray = as_array(S->elements, S->capacity);
+        S->abs = spec_malloc<struct AbsStack>();
+        S->abs->high_mark = S->topOfStack;
+        S->abs->capacity = S->capacity;)
+    wrap(S->elementsAsArray);
+    wrap(S->abs);
+    wrap(S);
+    return S;
 }
 
-
-void DisposeStack(struct Stack * S)
+void DisposeStack(struct Stack *S)
     requires(wrapped(S))
     writes(S)
 {
-    assert( in_domain( S->elementsAsArray, S ) );
+    assert(in_domain(S->elementsAsArray, S));
     unwrap(S);
-
-    assert( set_in( S->elementsAsArray, old(owns(S))));
-    unwrap( S->elementsAsArray );
-    assert( mutable( S->elementsAsArray ) );
-    free( (void*) S->elementsAsArray  );
-
-
-speconly(
-    unwrap( S->abs );
-)
-
+    unwrap(S->elementsAsArray);
+    free((void*) as_array(S->elements,S->capacity));
+    unwrap(S->abs);
     free(S);
 }
-void MakeEmpty(struct Stack * S)
-    requires(wrapped(S))
-    ensures(wrapped(S))
+
+void MakeEmpty(struct Stack *S)
+    maintains(wrapped(S))
     ensures(IsEmpty(S))
     writes(S)
 {
     unwrap(S);
     S->topOfStack = 0;
-speconly(
-	unwrap( S->abs );
-	S->abs->high_mark = S->topOfStack;
-	wrap( S->abs );
-)
+    unwrap(S->abs);
+    speconly(S->abs->high_mark = S->topOfStack;)
+    wrap(S->abs);
     wrap(S);
 }
 
-
-size_t Top(struct Stack * S)
-    requires(!IsEmpty(S))
+size_t Top(struct Stack *S)
     requires(wrapped(S))
-	returns( S->abs->entries[S->abs->high_mark - 1] )
-    reads(S)
+    requires(!IsEmpty(S))
+    returns(S->abs->entries[S->abs->high_mark - 1])
 {
-    assert( in_domain(S->elementsAsArray, S));
-    return S->elements[ S->topOfStack-1 ];
+    assert(in_domain(S->elementsAsArray, S));
+    return S->elements[S->topOfStack - 1];
 }
 
-void Pop(struct Stack * S)
-    requires(wrapped(S))  
-    ensures(wrapped(S))
-    requires(!IsEmpty(S))
+void Pop(struct Stack *S)
+    maintains(wrapped(S))
     writes(S)
+    requires(!IsEmpty(S))
     ensures(!IsFull(S))
-    ensures( S->abs->high_mark == old(S->abs->high_mark) - 1 )
-	ensures( forall( size_t idx;
-					 (0 <= idx) && ( idx < S->abs->high_mark ) 
-						  ==> (old(S->abs->entries[idx]) == S->abs->entries[idx] ) ) )
+    ensures(S->abs->high_mark == old(S->abs->high_mark) - 1)
+    ensures(forall(size_t idx; idx < S->abs->high_mark;
+        unchanged(S->abs->entries[idx])))
 {
     unwrap(S);
-    S->topOfStack = S->topOfStack-1;
-speconly(
-	unwrap( S->abs );
-	S->abs->high_mark--;
-	wrap( S->abs );
-)
+    S->topOfStack--;
+    unwrap(S->abs);
+    speconly(S->abs->high_mark--;)
+    wrap(S->abs);
     wrap(S);
 }
 
-void Push(struct Stack * S, size_t X)
-    requires(wrapped(S))  
-    ensures(wrapped(S))
+void Push(struct Stack *S, size_t X)
+    maintains(wrapped(S))
+    writes(S)
     requires(!IsFull(S))
     ensures(!IsEmpty(S))
-	ensures( old(S->abs->high_mark) + 1 == S->abs->high_mark ) 
-	ensures( forall( size_t idx;
-					 (0 <= idx) && ( idx < old(S->abs->high_mark) ) 
-						  ==> (old(S->abs->entries[idx]) == S->abs->entries[idx] ) ) )
-	ensures( S->abs->entries[S->abs->high_mark - 1] == X )
-    writes(S)
+    ensures(S->abs->high_mark == old(S->abs->high_mark) + 1)
+    ensures(forall(size_t idx; idx < old(S->abs->high_mark);
+        unchanged(S->abs->entries[idx])))
+    ensures(S->abs->entries[S->abs->high_mark - 1] == X)
 {
-    assert( S->topOfStack < S->capacity );
-    assert( in_domain(S->elementsAsArray, S));
+    assert(in_domain(S->elementsAsArray, S));
     unwrap(S);
-    assert( set_in( S->elementsAsArray, old(owns(S))));
-    unwrap( S->elementsAsArray );
-    S->elements[ S->topOfStack ] = X;
-    wrap( S->elementsAsArray );
-    S->topOfStack = S->topOfStack+1;
-speconly(
-	unwrap( S->abs );
-	assert( old( S->topOfStack ) == S->abs->high_mark );
-	S->abs->entries[S->abs->high_mark] = X;
-	S->abs->high_mark = S->topOfStack;
-	wrap( S->abs );
-)
-    wrap(S);      
+    unwrap(S->elementsAsArray);
+    S->elements[S->topOfStack] = X;
+    wrap(S->elementsAsArray);
+    S->topOfStack++;
+    unwrap(S->abs);
+    speconly(
+        S->abs->entries[S->abs->high_mark] = X;
+        S->abs->high_mark++;
+    )
+    wrap(S->abs);
+    wrap(S);
 }
 
-
-no_reads_check ispure
-bool IsIn(struct Stack * S, size_t value)
+ispure
+bool IsIn(struct Stack *S, size_t value)
     requires(wrapped(S))
-    reads(S)
-    ensures(result <==> exists(size_t v; (0 <= v) && (v < S->abs->high_mark) && S->abs->entries[v] == value))
+    reads(set_universe())
+    returns(exists(size_t v; v < S->abs->high_mark && S->abs->entries[v] == value))
 {
-    unsigned int index;
-    bool return_value = false;
+    unsigned index;
 
-    assert( in_domain(S->elementsAsArray, S));
-    assert( in_domain(S->abs, S) );
+    assert(in_domain(S->elementsAsArray, S));
+    assert(in_domain(S->abs, S));
 
-    for ( index = 0; index < S->topOfStack; index++ ) 
-        invariant( 0 <= index )
-        invariant( index <= S->topOfStack )
-        invariant( return_value ==> (exists(size_t v; (0 <= v) && (v < index); (S->abs->entries[v] == value))))
-        invariant( !return_value ==> (forall(size_t v; (0 <= v) && (v < index); (S->abs->entries[v] != value))))
+    for (index = 0; index < S->topOfStack; index++)
+        invariant(forall(size_t v; v < index; S->abs->entries[v] != value))
     {
-        if ( S->elements[index] == value ) {
-            assert( S->abs->entries[index] == value);
-            return_value = true;
-        }
+        if (S->elements[index] == value)
+            return 1;
     }
-    return return_value;
+    return 0;
 }
 
 /*`
