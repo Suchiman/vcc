852d269910156a1a561beb85e05aa5e124f91672
stobies <stobies@localhost>
2009-09-28 13:34:54 +0000

    Removing dead code from VisitorHelper

--- vcc/CodeModel2VccHelper/VisitorHelper.cs
+++ vcc/CodeModel2VccHelper/VisitorHelper.cs
@@ -5,7 +5,6 @@
 //-----------------------------------------------------------------------------
 using System;
 using System.Collections.Generic;
-using BoundExpression = Microsoft.Cci.MutableCodeModel.BoundExpression;
 using Microsoft.Cci;
 using Microsoft.Boogie;
 
@@ -13,47 +12,6 @@ namespace Microsoft.Research.Vcc
 {
   public static class VisitorHelper
   {
-
-    public static string IntegerTypeSuffix(ITypeReference type)
-    {
-      if (type.ResolvedType.IsEnum) type = type.ResolvedType.UnderlyingType;
-
-      switch (type.TypeCode) {
-        case PrimitiveTypeCode.Char:
-        case PrimitiveTypeCode.UInt8:
-        case PrimitiveTypeCode.UInt16:
-        case PrimitiveTypeCode.UInt32:
-          return ".u4";
-        case PrimitiveTypeCode.UInt64:
-          return ".u8";
-        case PrimitiveTypeCode.Int8:
-        case PrimitiveTypeCode.Int16:
-        case PrimitiveTypeCode.Int32:
-          return ".i4";
-        case PrimitiveTypeCode.Int64:
-          return ".i8";
-        default:
-          throw new InvalidOperationException("integer type required");
-      }
-    }
-
-    public static IExpression GetReturnValue(ISourceMethodBody methodBody)
-    {
-      foreach (IStatement statement in methodBody.Block.Statements) {
-        IReturnStatement/*?*/ retStatement = statement as IReturnStatement;
-        if (retStatement != null) {
-          IExpression retval = retStatement.Expression;
-          if (retval != null) return retval;
-        }
-      }
-      return CodeDummy.Expression;
-    }
-
-    public static Token GetTokenFor(IEnumerable<ILocation> locations)
-    {
-      return VisitorHelper.GetTokenFor(locations, false);
-    }
-
     private class DeferredToken
     {
       private readonly IEnumerable<ILocation> locations;
@@ -84,81 +42,10 @@ namespace Microsoft.Research.Vcc
       }
     }
 
-    public static Token GetTokenFor(IEnumerable<ILocation> locations, bool useEndOfLocation)
+    public static Token GetTokenFor(IEnumerable<ILocation> locations)
     {
       if (IteratorHelper.EnumerableIsEmpty(locations)) return Token.NoToken;
       return new LazyToken(new DeferredToken(locations).GetToken);
-    }
-
-    public static int RoundToBvSize(ITypeDefinition type)
-    {
-      return TypeHelper.SizeOfType(type) * 8 <= 32 ? 32 : 64;
-    }
-
-    public static bool TryInterpretAsFixedSizeArrayAccess(
-      IAddressDereference addressDereference,
-      out ITypeDefinition arrayType,
-      out ITypeDefinition elementType,
-      out IBoundExpression boundExpr,
-      out IExpression offsetAndScale)
-    {
-      arrayType = null;
-      elementType = null;
-      boundExpr = null;
-      offsetAndScale = null;
-      IAddition ptrAddition = addressDereference.Address as IAddition;
-      if (ptrAddition == null) return false;
-      IConversion convToElementPtrType = ptrAddition.LeftOperand as IConversion;
-      if (convToElementPtrType == null) return false;
-      IConversion convToVoidStar = convToElementPtrType.ValueToConvert as IConversion;
-      if (convToVoidStar == null) return false;
-      IAddressOf addressOf = convToVoidStar.ValueToConvert as IAddressOf;
-      if (addressOf == null) return false;
-      IAddressableExpression addressableExpr = addressOf.Expression as IAddressableExpression;
-      if (addressableExpr == null) return false;
-      // if we have come this far, we know that the original expresion has been of the form
-      // s.a[i], translate it accordingly
-      IFieldDefinition fieldDef = addressableExpr.Definition as IFieldDefinition;
-      if (fieldDef == null) return false;
-      ITypeDefinition fieldType = fieldDef.Type.ResolvedType;
-      if (!IsFixedSizeArray(fieldType)) return false;
-      arrayType = fieldType;
-      elementType = GetFixedSizeArrayElementType(fieldType);
-      BoundExpression bndExpr = new BoundExpression();
-      bndExpr.Definition = addressableExpr.Definition;
-      bndExpr.Instance = addressableExpr.Instance;
-      boundExpr = bndExpr;
-      offsetAndScale = ptrAddition.RightOperand;
-      return true;
-    }
-
-    public static bool TryInterpretAsFixedSizeArrayAccess(IExpression expr, out IBoundExpression boundExpr)
-    {
-      boundExpr = null;
-      IAddressDereference addressDereference = expr as IAddressDereference;
-      if (addressDereference == null) return false;
-      ITypeDefinition arrayType;
-      ITypeDefinition elementType;
-      IExpression offsetAndScale;
-      return TryInterpretAsFixedSizeArrayAccess(addressDereference, out arrayType, out elementType, out boundExpr, out offsetAndScale);
-    }
-
-    public static bool IsFixedSizeArray(ITypeDefinition parType)
-    {
-      if (parType.SizeOf == 0) return false;
-      INestedTypeDefinition/*?*/ nestedType = parType as INestedTypeDefinition;
-      if (nestedType == null) return false;
-      return nestedType.Name.Value.StartsWith("_FixedArrayOfSize");
-    }
-
-    public static ITypeDefinition GetFixedSizeArrayElementType(ITypeDefinition fixedSizeArrayType)
-    {
-      foreach (ITypeDefinitionMember member in fixedSizeArrayType.Members) {
-        IFieldDefinition/*?*/ field = member as IFieldDefinition;
-        if (field != null && field.Name.Value == "_ElementType")
-          return field.Type.ResolvedType;
-      }
-      return null;
     }
 
     public static ISourceLocation LocationFromToken(Token tok)
