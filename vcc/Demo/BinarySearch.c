d897eb3231fa267896758575baa6e38a0c76e653
stobies <stobies@localhost>
2009-11-10 14:11:58 +0000

    Full specification of the binary search algorithm

--- vcc/Demo/BinarySearch.c
+++ vcc/Demo/BinarySearch.c
@@ -5,18 +5,18 @@ unsigned binary_search(int val, int *arr, unsigned len)
   requires(is_mutable_array(arr, len))
   requires(forall(unsigned i,j; i < j && j < len ==> arr[i] <= arr[j]))
   ensures(result != UINT_MAX ==> arr[result] == val)
-  //ensures(result == UINT_MAX ==> forall(unsigned i; arr[i] != val))
+  ensures(result == UINT_MAX ==> forall(unsigned i; i < len ==> arr[i] != val))
 {
   unsigned low, high, mid;
   low = 0; high = len;
   while (low < high)
     invariant(high <= len)
-    //invariant(forall(unsigned i; i < low ==> arr[i] != val))
-    //invariant(forall(unsigned i; high <= i && i < len ==> arr[i] != val))
+    invariant(forall(unsigned i; i < low              ==> arr[i] <  val))
+    invariant(forall(unsigned i; high <= i && i < len ==> arr[i] >= val))
   {
     mid = low + (high - low) / 2;
-    if (arr[mid] < val) low = mid + 1;
-    else                high = mid;
+    if (arr[mid] < val)             low = mid + 1;
+    else                            high = mid;
   }
 
   if (low < len && arr[low] == val) return low;
