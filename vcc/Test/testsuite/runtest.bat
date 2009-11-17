38077e655548ad4dd97fcea91346b415e092fc68
stobies <stobies@localhost>
2009-11-17 14:17:13 +0000

    Moved desugaring of lambdas to after removal of memory assignments as we need correct type information Fixed detection of side-effects in lambdas to scan the generated bodies, not the input, for side effects. Fixes issue #3350.

--- vcc/Test/testsuite/runtest.bat
+++ vcc/Test/testsuite/runtest.bat
@@ -1,3 +1,3 @@
 echo "Refresh file to see test results" > ts.out
 start n ts.out
-..\..\Host\bin\debug\vcc.exe /b:/smoke /s vcc2 done examples > ts.out
+..\..\Host\bin\debug\vcc.exe /b:/smoke /s vcc2 done examples /smt:3 > ts.out
