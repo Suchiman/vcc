94155f337635e463504132a30030eb6ecb04598d
MichalMoskal <MichalMoskal@localhost>
2010-01-29 20:20:31 +0000

    Add some more content on function contracts

--- vcc/Docs/Tutorial/c/01_min4.c
+++ vcc/Docs/Tutorial/c/01_min4.c
@@ -1,13 +1,12 @@
-/*{min}*/
 #include <vcc.h>
 
+/*{min}*/
 int min(int a, int b)
   requires( true )
   ensures( result <= a && result <= b )
 {
-  return a < b ? a : b;
+  return a > b ? a : b;
 }
-// ... definition of main() unchanged ...
 /*{endmin}*/
 #define LIMIT 1000
 int main()
@@ -21,6 +20,8 @@ int main()
 }
 /*{out}*/
 /*`
-Verification of min succeeded.
+Verification of min failed.
+testcase(8,3) : error VC9501: Post condition 'result <= a && result <= b' did not verify.
+testcase(6,14) : error VC9599: (related information) Location of post condition.
 Verification of main succeeded.
 `*/
