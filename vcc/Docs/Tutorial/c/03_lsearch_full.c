8bcd334c0e5fe72c196ffd965673a5fb5e8e5b39
MichalMoskal <MichalMoskal@localhost>
2010-01-30 03:39:44 +0000

    More loops

--- vcc/Docs/Tutorial/c/03_lsearch_full.c
+++ vcc/Docs/Tutorial/c/03_lsearch_full.c
@@ -4,9 +4,12 @@
 unsigned lsearch(int elt, int *ar, unsigned sz)
   requires( wrapped( as_array( ar, sz ) ) )
   ensures(result != UINT_MAX ==> ar[result] == elt)
+  ensures(result == UINT_MAX ==> 
+            forall(unsigned i; i < sz ==> ar[i] != elt))
 {
   unsigned i;
   for (i = 0; i < sz; i++)
+    invariant(forall(unsigned j; j < i ==> ar[j] != elt))
   {
     if (ar[i] == elt) return i;
   }
