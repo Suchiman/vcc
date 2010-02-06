12aea09ed7fad535c3df202434ea487b599a446a
MichalMoskal <MichalMoskal@localhost>
2010-02-06 02:43:36 +0000

    Start with the converter to the new syntax

---
+++ vcc/Tools/VccSyntaxCoverter/Driver.fs
@@ -0,0 +1,23 @@
+﻿open Microsoft.Research.Vcc.SyntaxConverter
+open Microsoft.Research.Vcc.SyntaxConverter.Ast
+
+let go filename = 
+  try
+    let toks = Tokenizer.fromFile filename
+    let toks = Rules.apply [] toks
+    let outf = System.IO.File.CreateText (filename + ".out")
+    let outs = (Tok.Group (fakePos, "", toks)).ToString()
+    let outs = outs.Replace ("\n", "\r\n")
+    outf.Write (outs)
+    outf.Close()
+  with SyntaxError (p, s) ->
+    System.Console.WriteLine ("{0}:{1}: {2}", filename, p, s)
+    
+let main() =
+  Rules.init()
+  let args = System.Environment.GetCommandLineArgs()
+  for i = 1 to args.Length - 1 do
+    go args.[i]
+      
+
+main()
