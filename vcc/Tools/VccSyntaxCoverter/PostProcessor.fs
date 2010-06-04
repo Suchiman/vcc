93f616cc811fd164a35a008ef8324bce441026cd
stobies <stobies@localhost>
2010-06-04 15:03:19 +0000

    Fixed converted to properly add the /newsyntax test suite option in the presence of other options

--- vcc/Tools/VccSyntaxCoverter/PostProcessor.fs
+++ vcc/Tools/VccSyntaxCoverter/PostProcessor.fs
@@ -6,20 +6,21 @@ module PostProcessor =
 
   let apply addCompilerOptionForTestSuite toks = 
     let nl = Tok.Whitespace(fakePos, "\n") 
+    let rec addNewSyntaxOption  = function
+      | Whitespace _ as ws :: rest -> ws :: addNewSyntaxOption rest
+      | Comment(p, s) :: rest when addCompilerOptionForTestSuite && s.StartsWith("`/") 
+        -> Comment(p, s + "/newsyntax") :: nl :: rest
+      | toks -> Comment(fakePos, "`/newsyntax") :: nl :: toks
     let rec apply' acc = function
       | Tok.Id(p0, "_") :: Tok.Group(p1, "(", Tok.Id(p2, "ghost") :: Tok.Whitespace _ :: Tok.Id(p3, "out") :: gRest) :: rest ->
         apply' acc (Tok.Id(p0, "_") :: Tok.Group(p1, "(", Tok.Id(p3, "out") :: gRest) :: rest)
       | Tok.Comment(p, s) as tok :: rest when s.StartsWith("`") && s.[s.Length - 1] = '`' ->
         match Rules.eatWs rest with
          | [] -> apply' (tok :: acc) []
-         | _ -> apply' (Comment(fakePos, "`/newsyntax") :: nl :: tok :: acc) rest
+         | _ -> apply' (tok :: acc) (addNewSyntaxOption rest)
       | Tok.Group (p, s, toks) :: rest -> apply' (Tok.Group (p, s, apply' [] toks) :: acc) rest  
       | t :: rest -> apply' (t::acc) rest
       | [] -> List.rev acc
     let toks = apply' [] toks
-    match toks with
-      | Comment(p, s) :: rest when addCompilerOptionForTestSuite && s.StartsWith("`/") 
-        -> Comment(p, s + ";/newsyntax") :: Tok.Whitespace(fakePos, "\n") :: rest
-      | _ when addCompilerOptionForTestSuite -> Comment(fakePos, "`/newsyntax") :: nl :: toks
-      | _ -> toks
+    if addCompilerOptionForTestSuite then addNewSyntaxOption toks else toks
       
