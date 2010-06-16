e82ae8f35b0d20e93fa3a36a464b0d60d91392bd
MichalMoskal <MichalMoskal@localhost>
2010-06-16 23:20:47 +0000

    Improve explanations about ownership

--- vcc/Docs/Tutorial/c/06_safestring_dynamic_assert.c
+++ vcc/Docs/Tutorial/c/06_safestring_dynamic_assert.c
@@ -12,38 +12,36 @@ struct SafeString {
 };
 /*{append}*/
 void sstr_append_char(struct SafeString *s, char c)
+  _(requires \wrapped(s))
+  _(requires s->len < s->capacity - 1)
+  _(ensures \wrapped(s))
+  _(writes s)
 {
-  _(ghost \object cont;)
-
-  _(assume \wrapped(s))
-  _(assume s->len < s->capacity - 1)
-
-  _(ghost {
-    cont = (char[s->capacity])(s->content); // for readability
-    // unwrap s
-    _(assert \wrapped(s))
-    cont->theOwner = \me; // transfer 1
-    s->consistencyFlag = \false;
-    // unwrap cont
-    _(assert \wrapped(cont))
-    c->consistencyFlag = \false;
-  })
+  _(ghost \object cont = (char[s->capacity]) s->content; )
+  // _(unwrap s) steps 1-5
+  _(assert \writable(s) && \wrapped(s))
+  _(assume \writable(span(s)) && \inv(s))
+  _(ghost s->consistencyFlag = \false; )
+  // and the transfer:
+  _(ghost cont->\owner = \me; )
+  _(assume \writable(cont))
+  // _(unwrap cont) steps 1-5
+  _(assert \writable(cont) && \wrapped(cont))
+  _(ghost cont->consistencyFlag = \false; )
+  _(assume \writable(span(cont)) && \inv(cont))
+  // no transfer here
   
   s->content[s->len++] = c;
   s->content[s->len] = '\0';
-  _(ghost {
-    // wrap c
-    _(assert \unwrapped(cont))
-    _(assert \inv(cont))
-    cont->consistencyFlag = \true;
-    // wrap s
-    _(assert \unwrapped(s))
-    cont->theOwner = s; // transfer 2
-    _(assert \inv(s))
-    s->consistencyFlag = \true;
-    // ensures
-    _(assert \wrapped(s))
-  })
+
+  // _(wrap cont) steps 1-3
+  _(assert \unwrapped(cont) && \inv(cont))
+  _(ghost cont->consistencyFlag = \true; )
+  // _(wrap s) steps 1-3, with transfer in the middle
+  _(assert \unwrapped(s))
+  _(ghost cont->\owner = s; )
+  _(assert \inv(s))
+  _(ghost s->consistencyFlag = \true; )
 }
 /*{out}*/
 // Not that the output makes so much sense, but at least there are no parse errors.
