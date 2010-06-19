2da8771ff50dfb2e847a74d51c025c81662e4c74
MichalMoskal <MichalMoskal@localhost>
2010-06-19 00:56:01 +0000

    New sorting algorithms; add output

--- vcc/Docs/Tutorial/c/04_bozosort_perm.c
+++ vcc/Docs/Tutorial/c/04_bozosort_perm.c
@@ -15,7 +15,9 @@ _(logic bool is_permuted(\state s, int *buf, unsigned len, perm_t perm) =
 _(_(pure) perm_t swap(perm_t p, unsigned i, unsigned j)
   _(ensures \result == \lambda unsigned k; k == i ? p[j] : k == j ? p[i] : p[k]); )
 
-void insertion_sort(int *buf, unsigned len _(out perm_t perm))
+unsigned random();
+
+void bozo_sort(int *buf, unsigned len _(out perm_t perm))
   _(writes \array_range(buf, len))
   _(ensures sorted(buf, len))
   _(ensures is_permutation(perm, len))
@@ -24,44 +26,35 @@ void insertion_sort(int *buf, unsigned len _(out perm_t perm))
   unsigned i, j;
   int v;
   _(ghost \state s0 = \current_state() )
-  _(ghost perm_t perm2 )
 
   _(ghost perm = \lambda unsigned i; i) 
 
-  for (i = 1; i < len; ++i)
-    _(invariant sorted(buf, i))
+  if (len == 0) return;
+
+  for (;;)
     _(invariant \mutable_array(buf, len))
     _(invariant is_permutation(perm, len))
     _(invariant is_permuted(s0, buf, len, perm))
   {
-    _(ghost unsigned tmp = perm[i])
-    v = buf[i];
-    j = i - 1;
-    _(ghost perm2 = perm )
-    for (;;) 
-      _(invariant is_permutation(perm2, len))
-      _(invariant \forall unsigned k; perm2[k] == (k == j + 1 ? tmp : perm[k]))
-      _(invariant is_permuted(s0, buf, len, perm))
-      _(invariant j <= i - 1)
-      _(invariant sorted(buf, i))
-      _(invariant j == i - 1 || buf[i - 1] <= buf[i])
-      _(invariant \forall unsigned k; j < k && k <= i ==> buf[k] >= v)
-      _(invariant \mutable_array(buf, len))
+    int tmp;
+    unsigned i = random() % len; 
+    unsigned j = random() % len; 
+
+    tmp = buf[i];
+    buf[i] = buf[j];
+    buf[j] = tmp;
+    _(ghost perm = swap(perm, i, j) )
+
+    for (i = 0; i < len - 1; ++i)
+      _(invariant sorted(buf, i + 1))
     {
-      if (buf[j] > v) {
-        buf[j + 1] = buf[j];
-        _(ghost perm[j + 1] = perm[j] )
-        _(ghost perm2 = swap(perm2, j, j + 1) )
-        if (_(unchecked)(j--) == 0) break;
-      } else 
-        break;
+      if (buf[i] > buf[i + 1]) break;
     }
-    buf[_(unchecked)(j + 1)] = v;
-    _(ghost perm[_(unchecked)(j + 1)] = tmp)
-    _(assert perm2 == perm)
+
+    if (i == len - 1) break;
   }
 }
 
 /*`
-Verification of insertion_sort succeeded.
+Verification of bozo_sort succeeded.
 `*/
