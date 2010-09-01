65548b917213696a8c0b5b15d2dac8b52e12ea38
stobies <stobies@localhost>
2010-09-01 09:51:51 +0000

    Do not redirect test output to separate file Run with /smt switch instead

--- vcc/Test/testsuite/runtest.bat
+++ vcc/Test/testsuite/runtest.bat
@@ -1,4 +1,1 @@
-echo // Beginning of tests; refresh file to see test results > ts_out.txt
-start ts_out.txt
-..\..\Host\bin\debug\vcc.exe /smoke /s /time vcc2 done examples tutorial >> ts_out.txt
-echo // End of tests >> ts_out.txt
+..\..\Host\bin\debug\vcc.exe /smoke /s /time vcc2 done examples tutorial /smt
