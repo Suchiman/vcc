9513bb723ef499d5b1028cc0d6abac5a46e92e32
MichalMoskal <MichalMoskal@localhost>
2010-09-01 01:09:11 +0000

    Add Heap.c benchmark.

--- vcc/Plugins/Vcc2Plugins/Vcc3/bench/config.sh
+++ vcc/Plugins/Vcc2Plugins/Vcc3/bench/config.sh
@@ -1,7 +1,7 @@
-BENCHMARKS="List.c RedBlackTrees.c"
+BENCHMARKS="List.c RedBlackTrees.c Heap.c"
+#BENCHMARKS="Heap.c"
 FELT=c:/dev/felt
 VCC=$FELT/Vcc/Host/bin/Debug/vcc.exe
 Z3=$FELT/Vcc/Boogie/z3.exe
+THR=-z:QI_EAGER_THRESHOLD=1000 # not used
 COMMON_OPTS="-st -it -z:CASE_SPLIT=5 -z:/t:5"
-
-
