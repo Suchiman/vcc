dbbebfabc6f617e9e466070c813c76718be12cea
christoph89 <christoph89@localhost>
2010-11-24 11:26:19 +0000

    Initial Version of VSPackage created

---
+++ VSPackage/PkgCmdID.cs
@@ -0,0 +1,11 @@
+﻿// PkgCmdID.cs
+// MUST match PkgCmdID.h
+using System;
+
+namespace MicrosoftResearch.VSPackage
+{
+    static class PkgCmdIDList
+    {
+        public const uint cmdidMyCommand =        0x100;
+    };
+}
