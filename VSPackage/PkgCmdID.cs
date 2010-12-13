60d923b0708a87a763735fd5b16970b83b0f2d96
christoph89 <christoph89@localhost>
2010-12-13 16:40:55 +0000

    Added Contextmenu Commands for Verify File/Function, Added VCC Properties Page to Tools Options

--- VSPackage/PkgCmdID.cs
+++ VSPackage/PkgCmdID.cs
@@ -6,10 +6,14 @@ namespace MicrosoftResearch.VSPackage
 {
     static class PkgCmdIDList
     {
-        public const uint cmdidMyCommand =        0x100;
-        public const uint cmdidVerifyActiveFile = 0x102;
-        public const uint cmdidVerifyCurrentFunction = 0x0104;
-        public const uint cmdidVerifyMenu = 0x1021;
+        public const uint cmdidMyCommand =                      0x0100;
+        public const uint cmdidVerifyActiveFile =               0x0102;
+        public const uint cmdidVerifyCurrentFunction =          0x0104;
+
+        public const uint cmdidContextVerifyActiveFile =        0x0106;
+        public const uint cmdidContextVerifyCurrentFunction =   0x0108;
+
+        public const uint cmdidVerifyMenu =                     0x1021;
 
     };
 }
