790cb510ff0dad79ca7e12c0035b9ed15812697b
christoph89 <christoph89@localhost>
2010-12-22 09:55:57 +0000

    Added Cancel

--- VSPackage/PkgCmdID.cs
+++ VSPackage/PkgCmdID.cs
@@ -1,6 +1,5 @@
 ﻿// PkgCmdID.cs
 // MUST match PkgCmdID.h
-using System;
 
 namespace MicrosoftResearch.VSPackage
 {
@@ -9,9 +8,11 @@ namespace MicrosoftResearch.VSPackage
         public const uint cmdidMyCommand =                      0x0100;
         public const uint cmdidVerifyActiveFile =               0x0102;
         public const uint cmdidVerifyCurrentFunction =          0x0104;
+        public const uint cmdidCancel =                         0x0105;
 
         public const uint cmdidContextVerifyActiveFile =        0x0106;
         public const uint cmdidContextVerifyCurrentFunction =   0x0108;
+        public const uint cmdidContextCancel =                  0x0110;
 
         public const uint cmdidVerifyMenu =                     0x1021;
 
--- VSPackage/VCCLauncher.cs
+++ VSPackage/VCCLauncher.cs
@@ -1,61 +1,49 @@
 ﻿using System;
-using Microsoft.VisualStudio.Shell.Interop;
-using EnvDTE;
 using System.Diagnostics;
-using Microsoft.VisualStudio.Shell;
 using Process = System.Diagnostics.Process;
+using Thread = System.Threading.Thread;
+using System.Management;
 
 namespace MicrosoftResearch.VSPackage
 {
-    public static class VCCLauncher
+    /// <summary>
+    ///     This class is used to start VCC.exe with the correct parameters and to get the results from VCC.exe
+    /// </summary>
+    internal static class VCCLauncher
     {
         private static string vccPath = @"C:\Users\t-chworr\VCC\vcc\Host\bin\Debug\vcc.exe";
-        
-        private static Lazy<IVsOutputWindowPane> _pane = new Lazy<IVsOutputWindowPane>(() =>
-        {
-            IVsOutputWindow outputwindow = (IVsOutputWindow)Package.GetGlobalService(typeof(SVsOutputWindow));
-            Guid guidVerificationPane = new Guid("{1EE5916F-A3C7-403C-89D8-58C61285688F}");
-            IVsOutputWindowPane result;
-            
-            outputwindow.CreatePane(ref guidVerificationPane, "Verification", 1, 1);
-            outputwindow.GetPane(ref guidVerificationPane, out result);
-            return result;
-        });
+        private static Process vccProcess;
 
-        static IVsOutputWindowPane pane
+        internal static void VerifyFile(string filename, VccOptionPage options)
         {
-            get { return _pane.Value; }
+            string addArguments = options.UseAdditionalCommandlineArguments ? options.AdditionalCommandlineArguments + " " : string.Empty;
+            LaunchVCC(String.Format("{0}\"{1}\"", addArguments, filename));
         }
 
-        public static void VerifyFile(string filename, string addArguments)
+        internal static void VerifyFunction(string filename, string function, VccOptionPage options)
         {
-            LaunchVCC(String.Format("{0} \"{1}\"", addArguments, filename));
+            string addArguments = options.UseAdditionalCommandlineArguments ?
+                options.AdditionalCommandlineArguments + " " :
+                string.Empty;
+            LaunchVCC(String.Format("{0} /f:\"{1}\" \"{2}\"", addArguments, function, filename));
         }
 
-        public static void VerifyFunction(string filename, string function, string addArguments)
+        internal static void LaunchVCC(string arguments)
         {
-            LaunchVCC(String.Format("{0} /F:\"{1}\" \"{2}\"", addArguments, function, filename));
-        }
-
-        public static void LaunchVCC(string arguments)
-        {
-            //// get the toplevel object for interaction with VS
-            DTE dte = VSIntegration.dte;
-            
             //// Prepare VCC-Process, execute it and read its Output
                         
             ProcessStartInfo psi = new ProcessStartInfo(string.Format("\"{0}\"",vccPath), arguments);
             psi.UseShellExecute = false;
             psi.RedirectStandardOutput = true;
             psi.CreateNoWindow = true;
-            Process vccProcess = new Process();
+            vccProcess = new Process();
             vccProcess.StartInfo = psi;
 
             //// Clear Verification Outputpane
-            pane.Clear();
-            pane.OutputString("===VCC started.===\n");
+            VSIntegration.ClearPane();
+            VSIntegration.WriteToPane("===VCC started.===\n");
             //// Write Commandline-Command to Verification Outputpane
-            pane.OutputString(string.Format("Command Line: \"{0}\" {1}\n\n", vccPath, arguments));
+            VSIntegration.WriteToPane(string.Format("Command Line: \"{0}\" {1}\n\n", vccPath, arguments));
             //// Get notified when VCC sends Output or Error Data
             vccProcess.OutputDataReceived += new DataReceivedEventHandler(vccProcess_OutputDataReceived);
             vccProcess.ErrorDataReceived += new DataReceivedEventHandler(vccProcess_OutputDataReceived);
@@ -68,18 +56,79 @@ namespace MicrosoftResearch.VSPackage
             vccProcess.BeginOutputReadLine();
         }
 
-        static void vccProcess_Exited(object sender, EventArgs e)
+        private static void vccProcess_Exited(object sender, EventArgs e)
         {
-            pane.OutputString("===VCC finished.===\n");
-        }
-
-        static void vccProcess_OutputDataReceived(object sender, DataReceivedEventArgs e)
-        {
-            // Write Output from VCC to Verification Outputpane
-            if (pane != null)
+            if (vccProcess != null && vccProcess.ExitCode == -1)
             {
-                pane.OutputString(String.Format("{0}\n", e.Data));
+                VSIntegration.WriteToPane("===VCC was canceled.===\n");
+            }
+            else
+            {
+                VSIntegration.WriteToPane("===VCC finished.===\n");
             }
         }
+
+        private static void vccProcess_OutputDataReceived(object sender, DataReceivedEventArgs e)
+        {
+            // Write Output from VCC to Verification Outputpane
+            if (VCCRunning())
+            {
+                VSIntegration.WriteToPane(String.Format("{0}\n", e.Data));
+            }
+        }
+
+        private static int GetParentProcess(int Id)
+        {
+            int parentPid = 0;
+            using (ManagementObject mo = new ManagementObject("win32_process.handle='" + Id.ToString() + "'"))
+            {
+
+                try
+                {
+                    mo.Get();
+                    parentPid = Convert.ToInt32(mo["ParentProcessId"]);
+                }
+                catch { }
+            }
+            return parentPid;
+        }
+
+        /// <summary>
+        ///     Cancels the running VCC Process, if it exists
+        /// </summary>
+        internal static void Cancel()
+        {
+            if (VCCRunning())
+            {
+                try
+                {
+                    vccProcess.Kill();
+                    foreach (Process subProcess in Process.GetProcesses())
+                    {
+                        if (GetParentProcess(subProcess.Id) == vccProcess.Id)
+                        {
+                            subProcess.Kill();
+                        }//if
+                    }//foreach
+                }//try
+                catch
+                {
+                    VSIntegration.WriteToPane("Canceling VCC failed.");
+                }//catch
+            }//if
+        }//method
+
+        internal static bool VCCRunning()
+        {
+            if (vccProcess != null && !vccProcess.HasExited)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
     }
 }
