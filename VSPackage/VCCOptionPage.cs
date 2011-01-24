2bcb1279e63334180d8899a52ffaf547ceb59127
christoph89 <christoph89@localhost>
2011-01-24 16:46:56 +0000

    fixed svn errors -.-

---
+++ VSPackage/VCCOptionPage.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel;
+using Microsoft.VisualStudio.Shell;
+
+namespace MicrosoftResearch.VSPackage
+{
+
+    public class VccOptionPage : DialogPage
+    {
+        [DisplayName("Commandline Arguments")]
+        [Description("Here you can place additional commandline arguments for VCC that will be used every time VCC is executed.")]
+        public string AdditionalCommandlineArguments { get; set; }
+
+        [DisplayName("Use Commandline Arguments")]
+        [Description("Choose true to use the arguments you entered above. If this is false, those arguments will be ignored.")]
+        public bool UseAdditionalCommandlineArguments { get; set; }
+
+        [DisplayName("Show Z3 Inspector")]
+        [Description("Choose true to launch the Z3 Inspector to view the progress of verification.")]
+        public bool showZ3Inspector{ get; set; }
+
+    }
+}
