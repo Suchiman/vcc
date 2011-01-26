de3e6fd972ef6b4fecc5bd9bde2fcf6a20fef67b
stobies <stobies@localhost>
2011-01-26 15:36:49 +0000

    New try to get the dialog to adjust better to non-standard DPI values

--- vcc/AddIn/VCCAddin/Forms/CustomVerify/CustomVerifyForm.Designer.cs
+++ vcc/AddIn/VCCAddin/Forms/CustomVerify/CustomVerifyForm.Designer.cs
@@ -41,25 +41,28 @@ namespace VerifiedCCompilerAddin.Forms {
       // label1
       // 
       this.label1.AutoSize = true;
-      this.label1.Location = new System.Drawing.Point(13, 13);
+      this.label1.Location = new System.Drawing.Point(17, 16);
+      this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
       this.label1.Name = "label1";
-      this.label1.Size = new System.Drawing.Size(145, 13);
+      this.label1.Size = new System.Drawing.Size(195, 17);
       this.label1.TabIndex = 0;
       this.label1.Text = "Additional parameter for VCC:";
       // 
       // txtAddidtional
       // 
-      this.txtAddidtional.Location = new System.Drawing.Point(16, 29);
+      this.txtAddidtional.Location = new System.Drawing.Point(21, 36);
+      this.txtAddidtional.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
       this.txtAddidtional.Name = "txtAddidtional";
-      this.txtAddidtional.Size = new System.Drawing.Size(367, 20);
+      this.txtAddidtional.Size = new System.Drawing.Size(488, 22);
       this.txtAddidtional.TabIndex = 1;
       // 
       // button1
       // 
       this.button1.DialogResult = System.Windows.Forms.DialogResult.OK;
-      this.button1.Location = new System.Drawing.Point(308, 55);
+      this.button1.Location = new System.Drawing.Point(411, 68);
+      this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
       this.button1.Name = "button1";
-      this.button1.Size = new System.Drawing.Size(75, 23);
+      this.button1.Size = new System.Drawing.Size(100, 28);
       this.button1.TabIndex = 2;
       this.button1.Text = "&Verify";
       this.button1.UseVisualStyleBackColor = true;
@@ -68,9 +71,10 @@ namespace VerifiedCCompilerAddin.Forms {
       // cbFunction
       // 
       this.cbFunction.AutoSize = true;
-      this.cbFunction.Location = new System.Drawing.Point(16, 55);
+      this.cbFunction.Location = new System.Drawing.Point(21, 68);
+      this.cbFunction.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
       this.cbFunction.Name = "cbFunction";
-      this.cbFunction.Size = new System.Drawing.Size(118, 17);
+      this.cbFunction.Size = new System.Drawing.Size(150, 21);
       this.cbFunction.TabIndex = 3;
       this.cbFunction.Text = "Use active function";
       this.cbFunction.UseVisualStyleBackColor = true;
@@ -79,9 +83,10 @@ namespace VerifiedCCompilerAddin.Forms {
       // 
       this.pictureBox1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
       this.pictureBox1.Image = global::VerifiedCCompilerAddin.Properties.Resources.info;
-      this.pictureBox1.Location = new System.Drawing.Point(16, 87);
+      this.pictureBox1.Location = new System.Drawing.Point(21, 107);
+      this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
       this.pictureBox1.Name = "pictureBox1";
-      this.pictureBox1.Size = new System.Drawing.Size(16, 16);
+      this.pictureBox1.Size = new System.Drawing.Size(21, 20);
       this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
       this.pictureBox1.TabIndex = 0;
       this.pictureBox1.TabStop = false;
@@ -89,26 +94,28 @@ namespace VerifiedCCompilerAddin.Forms {
       // label2
       // 
       this.label2.AutoSize = true;
-      this.label2.Location = new System.Drawing.Point(38, 87);
+      this.label2.Location = new System.Drawing.Point(51, 107);
+      this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
       this.label2.Name = "label2";
-      this.label2.Size = new System.Drawing.Size(200, 13);
+      this.label2.Size = new System.Drawing.Size(264, 17);
       this.label2.TabIndex = 1;
       this.label2.Text = "The following switches are also included:";
       // 
       // label3
       // 
-      this.label3.Location = new System.Drawing.Point(38, 100);
+      this.label3.Location = new System.Drawing.Point(51, 123);
+      this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
       this.label3.Name = "label3";
-      this.label3.Size = new System.Drawing.Size(345, 32);
+      this.label3.Size = new System.Drawing.Size(460, 39);
       this.label3.TabIndex = 2;
       this.label3.Text = "label3 wrap me";
       // 
       // CustomVerifyForm
       // 
       this.AcceptButton = this.button1;
-      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+      this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
       this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-      this.ClientSize = new System.Drawing.Size(392, 86);
+      this.ClientSize = new System.Drawing.Size(523, 106);
       this.Controls.Add(this.label3);
       this.Controls.Add(this.label2);
       this.Controls.Add(this.cbFunction);
@@ -117,6 +124,7 @@ namespace VerifiedCCompilerAddin.Forms {
       this.Controls.Add(this.txtAddidtional);
       this.Controls.Add(this.label1);
       this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+      this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
       this.MaximizeBox = false;
       this.MinimizeBox = false;
       this.Name = "CustomVerifyForm";
