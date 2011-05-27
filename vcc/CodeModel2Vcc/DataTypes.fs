136d29e186ea51e3d065f4677b58729bdbf218cc
Michal Moskal <michal@moskal.me>
2011-05-26 19:02:04 -0700

    Check for grounding of datatypes (we cannot have infinite ones, as we assume each type is non-empty, and each datatype is finite)

--- vcc/CodeModel2Vcc/DataTypes.fs
+++ vcc/CodeModel2Vcc/DataTypes.fs
@@ -18,17 +18,29 @@ open Microsoft.Research.Vcc.CAST
 // - each datatype option is used at most once
 
 // for datatype definition check that
-// - attach constructors to type
-// - the induction is grounded (there is an non-recursive option)
+// - the induction is grounded (there is a non-recursive option)
+let checkDatatypeDefinitions (helper:Helper.Env) decls =
+  let cache = gdict()
+  let rec grounded (td:TypeDecl) =
+    match cache.TryGetValue td with
+      | true, res -> res
+      | _ ->
+        cache.Add (td, false)
+        let res = td.DataTypeOptions |> List.exists (fun fd -> fd.Parameters |> List.forall (fun p -> groundedType p.Type))
+        cache.[td] <- res
+        res
+  and groundedType = function
+    | Type.Ref td ->
+      not td.IsDataType || grounded td
+    | _ -> true
 
-// this needs to be done early, otherwise pruning will get rid of our constructors if they are not explicitly used, which would be bad
-let attachDatatypeCtors (helper:Helper.Env) decls =
-  let aux = function
-    | Top.FunctionDecl fd when fd.CustomAttr |> hasCustomAttr AttrAsArray ->
-      match fd.RetType with
-        | Type.Ref td when td.Kind = MathType ->
-          td.DataTypeOptions <- fd :: td.DataTypeOptions
-        | _ -> die()
+  let aux = function 
+    | Top.TypeDecl td when td.IsDataType ->
+      if not (grounded td) then
+        helper.Error (td.Token, 9725, "a finite instance of datatype '" + td.Name + "' could never be constructed")
     | _ -> ()
   List.iter aux decls
   decls
+
+let init (helper:Helper.Env) =
+  helper.AddTransformer ("datatype-check-defs", Helper.Decl (checkDatatypeDefinitions helper))
