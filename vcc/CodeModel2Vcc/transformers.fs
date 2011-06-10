7330ed6d4bc34cbc75bd6b9d0072d2f1ea4879b1
stobies <stobies@localhost>
2011-06-10 12:53:03 +0200

    work item: 6386 Report error for struct with fixed-size arrays that are used in equality assertions or assigments; they are supported in /3

--- vcc/CodeModel2Vcc/transformers.fs
+++ vcc/CodeModel2Vcc/transformers.fs
@@ -13,7 +13,7 @@ Verification errors:  between 8001 and 8499: First available: 8031
 Assertions:           between 8501 and 8999. First available: 8539
 Warnings:             between 9100 and 9199. First available: 9126
 Grave Warnings:       between 9300 and 9399. First available: 9319
-Errors:               between 9601 and 9799. First available: 9730
+Errors:               between 9601 and 9799. First available: 9731
 *)
 
 
