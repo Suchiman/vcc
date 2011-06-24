ef708dd71bc612cbbc916277eca044e41cf87348
Michal Moskal <michal@moskal.me>
2011-06-24 15:51:19 -0700

    Allow bit operations on math integers

---
+++ vcc/Demo/BitMap.c
@@ -0,0 +1,28 @@
+#include "BitMap.h"
+
+void InitializeBitMap (BITMAP *BitMap, unsigned int *BitMapBuffer, unsigned int Size)
+{
+  BitMap->Size = Size;
+  BitMap->Buffer = BitMapBuffer;
+  memzero(BitMapBuffer, Size/32);
+
+  spec(BitMap->BM = lambda(unsigned int i; i < Size; false);)
+  wrap(BitMap);
+}
+
+void SetBit (BITMAP *BitMap, unsigned int BitNumber)
+{
+  expose(BitMap) {
+  expose(as_array(BitMap->Buffer, BitMap->Size /32)) {
+    BitMap->Buffer[BitNumber/32] |= 1 << BitNumber % 32;
+    spec(BitMap->BM =
+       lambda(unsigned int i; true; i == BitNumber ? true : BitMap->BM[i]);)
+  }}
+}
+
+/*`
+Verification of _BITMAP#adm succeeded.
+Verification of InitializeBitMap succeeded.
+Verification of SetBit succeeded.
+Verification of TestBit#reads succeeded.
+`*/
---
+++ vcc/Demo/SpinLockNewSyntax.h
@@ -0,0 +1,29 @@
+#include <vcc.h>
+#include <stdlib.h>
+
+typedef _(claimable) _(volatile_owns) struct _SPIN_LOCK
+{
+  volatile int Lock;
+  _(ghost \object protected_obj)
+  _(invariant !Lock ==> \mine(protected_obj))
+} SPIN_LOCK;
+
+void InitializeSpinLock(SPIN_LOCK * SpinLock _(ghost \object obj))
+  _(writes \span(SpinLock), obj)
+  _(requires \wrapped(obj))
+  _(ensures \wrapped(SpinLock))
+  _(ensures SpinLock->protected_obj == obj)
+  ;
+
+void Acquire(SPIN_LOCK *SpinLock _(ghost \claim access_claim))
+  _(always access_claim, SpinLock->\consistent)
+  _(ensures \wrapped(SpinLock->protected_obj))
+  _(ensures \fresh(SpinLock->protected_obj))
+  ;
+
+void Release(SPIN_LOCK *SpinLock _(ghost \claim access_claim))
+  _(writes SpinLock->protected_obj)
+  _(always access_claim, SpinLock->\consistent)
+  _(requires access_claim != SpinLock->protected_obj)
+  _(requires \wrapped(SpinLock->protected_obj))
+  ;
