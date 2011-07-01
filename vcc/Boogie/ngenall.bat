6052969662d47174c564944cedb9e9b69fdf219c
Michal Moskal <michal@moskal.me>
2011-06-30 19:51:45 -0700

    Allow more things in _(def) functions; checks for lambdas and quantifiers are still missing.

---
+++ vcc/Boogie/ngenall.bat
@@ -0,0 +1,14 @@
+ngen AIFramework.dll
+ngen AbsInt.dll
+ngen BaseTypes.dll
+ngen Core.dll
+ngen FSharp.Core.dll
+ngen FSharp.PowerPack.dll
+ngen Graph.dll
+ngen Provers.Simplify.dll
+ngen Provers.Z3.dll
+ngen System.Compiler.Framework.dll
+ngen System.Compiler.Runtime.dll
+ngen System.Compiler.dll
+ngen VCExpr.dll
+ngen VCGeneration.dll
