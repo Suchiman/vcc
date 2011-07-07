a084fe81928c28a36576022be525ee23ce43fe0d
MarkHillebrand <MarkHillebrand@localhost>
2011-07-07 22:07:00 +0200

    vcc\Test\testsuite\tutorial: convert to new syntax

--- vcc/Test/testsuite/tutorial/rect_inv.c
+++ vcc/Test/testsuite/tutorial/rect_inv.c
@@ -1,26 +1,27 @@
+//`/newsyntax
 #include "vcc.h"
 
 struct point {
-    int x;
-    int y;
+  int x;
+  int y;
 };
 
 struct rect {
-    struct point ll;
-    struct point ur;
-	
-    invariant( ll.x <= ur.x && ll.y <= ur.y )
-    invariant( keeps( &ll ) )
-    invariant( keeps( &ur ) )
+  struct point ll;
+  struct point ur;
+
+  _(invariant ll.x <= ur.x && ll.y <= ur.y)
+  _(invariant \mine(&ll))
+  _(invariant \mine(&ur))
 };
 
-isadmissibilitycheck
-void custom_admissibility_check_rect( struct rect *r ) 
+_(admissibility)
+void custom_admissibility_check_rect(struct rect *r)
 {
-    // .. assert() and assume() 
-    havoc_others( r );
-    // .. assert() and assume() are allowed here. 
-    // The state or meta must not be modified.
+  // .. assert() and assume()
+  _(havoc_others  r)
+  // .. assert() and assume() are allowed here.
+  // The state or meta must not be modified.
 }
 
 /*`
