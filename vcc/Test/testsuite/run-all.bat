71def440511e0e56004ebb1118b79c7b0278e80d
Mark Hillebrand <Mark Hillebrand@localhost>
2011-07-07 09:44:43 +0200

    run-all.bat: add vacid-0 also to the vcc /3 targets

--- vcc/Test/testsuite/run-all.bat
+++ vcc/Test/testsuite/run-all.bat
@@ -6,4 +6,4 @@ copy testsuite.log testsuite-1.log
 REM NOTE: vcc3 must be run separatedly at this time, since /3 cannot be
 REM specified inside tests. Also note that Vcc3Prelude.bpl seems to be taken
 REM from the installation, and not from the build.
-..\..\Host\bin\debug\vcc.exe /smoke /s /time /smt:0.7 /3 vcc3 ..\..\Docs\Tutorial\c
+..\..\Host\bin\debug\vcc.exe /smoke /s /time /smt:0.7 /3 vcc3 vacid-0 ..\..\Docs\Tutorial\c
