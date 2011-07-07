8e408b019ac4b8531572bf6bb84b5d14fb074725
MarkHillebrand <MarkHillebrand@localhost>
2011-07-07 22:07:00 +0200

    vcc\Test\testsuite\tutorial: convert to new syntax

--- vcc/Test/testsuite/tutorial/rect_ptr.c
+++ vcc/Test/testsuite/tutorial/rect_ptr.c
@@ -1,48 +1,48 @@
+//`/newsyntax
 #include "vcc.h"
 
 struct point {
-    int x;
-    int y;
+  int x;
+  int y;
 };
 
 struct rect {
-    struct point *ll;
-    struct point *ur;
-	
-    invariant( inv_rect(this) )
-    invariant( keeps( ll ) )
-    invariant( keeps( ur ) )
-    invariant( ll != ur )
+  struct point *ll;
+  struct point *ur;
+
+  _(invariant inv_rect(\this))
+  _(invariant \mine(ll))
+  _(invariant \mine(ur))
+  _(invariant ll != ur)
 };
 
-spec( ispure bool inv_rect(struct rect * r)
-    reads(r)
-    returns(r->ll->x <= r->ur->x && r->ll->y <= r->ur->y);
-)
+_(ghost _(pure) bool inv_rect(struct rect *r)
+  _(reads r)
+  _(returns r->ll->x <= r->ur->x && r->ll->y <= r->ur->y))
 
-spec(ispure bool within_bounds(__in struct rect* r, int dx, int dy)
-    reads(r)
-    returns( 0 <= r->ll->x + dx && r->ll->x + dx < 1024 &&
-             0 <= r->ur->x + dx && r->ur->x + dx < 1024 &&
-             0 <= r->ll->y + dy && r->ll->y + dy < 1024 &&
-             0 <= r->ur->y + dy && r->ur->y + dy < 1024 );
-)
+_(ghost _(pure) bool within_bounds(struct rect *r, int dx, int dy)
+  _(reads r)
+  _(returns 0 <= r->ll->x + dx && r->ll->x + dx < 1024 &&
+    0 <= r->ur->x + dx && r->ur->x + dx < 1024 &&
+    0 <= r->ll->y + dy && r->ll->y + dy < 1024 &&
+    0 <= r->ur->y + dy && r->ur->y + dy < 1024))
 
-void move(__inout struct rect* r, int dx, int dy)
-    maintains(wrapped(r))
-    requires(within_bounds(r, dx, dy))
-	writes(r)
-{	
-    unwrap(r);
-    unwrap(r->ll);
-    unwrap(r->ur);
-    r->ll->x = unchecked(r->ll->x + dx);
-    r->ll->y = unchecked(r->ll->y + dy);
-    r->ur->x = unchecked(r->ur->x + dx);
-    r->ur->y = unchecked(r->ur->y + dy);
-    wrap(r->ur);
-    wrap(r->ll);
-    wrap(r);
+
+void move(struct rect *r, int dx, int dy)
+  _(maintains \wrapped(r))
+  _(requires within_bounds(r, dx, dy))
+  _(writes r)
+{
+  _(unwrap r)
+  _(unwrap r->ll)
+  _(unwrap r->ur)
+  r->ll->x = _(unchecked) (r->ll->x + dx);
+  r->ll->y = _(unchecked) (r->ll->y + dy);
+  r->ur->x = _(unchecked) (r->ur->x + dx);
+  r->ur->y = _(unchecked) (r->ur->y + dy);
+  _(wrap r->ur)
+  _(wrap r->ll)
+  _(wrap r)
 }
 
 /*`
