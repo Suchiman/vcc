59e678953f1a3b5b55e53a7b42b5d4916dcef8da
VccBuildServer <VccBuildServer@localhost>
2011-09-03 06:32:57 +0200

    Tagging CC.NET build 2.1.40903.0

---
+++ vcc/AddIn/VCCAddin/Commands/CustomCommands/VCCProgressCommand.cs
@@ -0,0 +1,36 @@
+﻿//-----------------------------------------------------------------------------
+//
+// Copyright (C) Microsoft Corporation.  All Rights Reserved.
+//
+//-----------------------------------------------------------------------------
+using System;
+using EnvDTE;
+using EnvDTE80;
+using VerifiedCCompilerAddin.Manager.Verify;
+
+// This is in invisible command, that is used internaly for callback functionality
+
+namespace VerifiedCCompilerAddin.Commands {
+  class VCCProgressCommand : VCCCommand {
+
+    public VCCProgressCommand(DTE2 dte, AddIn addin) :
+      base(dte, addin, "showVCCProgress", "Updates Progress", "Updates Progress", (int)VCCMenuIcons.CustomSingleFile,null,null) {      
+    }
+
+    public override bool Exec(EnvDTE.vsCommandExecOption executeOption, ref object varIn, ref object varOut, ref bool handled) {
+      handled = true;
+      //Progress Update!
+      if (VerifyManager.isRunning) {
+        VerifyManager.UpdateProgress(varIn as String);
+      }
+      
+      return true;
+    }
+
+    public override void QueryStatus(EnvDTE.vsCommandStatusTextWanted neededText, ref EnvDTE.vsCommandStatus status, ref object commandText) {
+      status = (EnvDTE.vsCommandStatus.vsCommandStatusEnabled | EnvDTE.vsCommandStatus.vsCommandStatusSupported);
+      commandText = "(internal)";
+    }
+
+  }
+}
