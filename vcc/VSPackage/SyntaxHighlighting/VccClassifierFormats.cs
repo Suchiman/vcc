21e24b978e716ed843b1cd477d4b81be09377aca
stobies <stobies@localhost>
2011-09-07 10:32:28 +0200

    Added simple syntax highlighting for spec code in VS2010

---
+++ vcc/VSPackage/SyntaxHighlighting/VccClassifierFormats.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.Composition;
+using System.Windows.Media;
+using Microsoft.VisualStudio.Text.Classification;
+using Microsoft.VisualStudio.Utilities;
+
+
+namespace Microsoft.Research.Vcc.VSPackage
+{
+    internal static class VccClassifierFormats
+    {
+        [Export(typeof (EditorFormatDefinition))]
+        [ClassificationType(ClassificationTypeNames = "vcc.spec")]
+        [Name("vcc.spec")]
+        internal sealed class VccSpecFormat : ClassificationFormatDefinition
+        {
+            public VccSpecFormat()
+            {
+                this.ForegroundOpacity = 0.5;
+                //this.BackgroundColor = Colors.BurlyWood;
+            }
+        }
+    }
+}
---
+++ vcc/VSPackage/SyntaxHighlighting/VccClassifierType.cs
@@ -0,0 +1,47 @@
+﻿using System.ComponentModel.Composition;
+using Microsoft.VisualStudio.Text.Classification;
+using Microsoft.VisualStudio.Utilities;
+
+namespace Microsoft.Research.Vcc.VSPackage
+{
+    internal static class VccClassificationTypeDefinitions
+    {
+
+        // http://msdn.microsoft.com/en-us/library/microsoft.visualstudio.language.standardclassification.predefinedclassificationtypenames.aspx
+
+
+        [Export]
+        [Name("vcc")]
+        [BaseDefinition(Microsoft.VisualStudio.Language.StandardClassification.PredefinedClassificationTypeNames.Comment)]
+// ReSharper disable RedundantDefaultFieldInitializer
+        internal static ContentTypeDefinition vccContentTypeDefinition = null;
+// ReSharper restore RedundantDefaultFieldInitializer
+
+        [Export]
+        [FileExtension(".c")]
+        [ContentType("vcc")]
+// ReSharper disable RedundantDefaultFieldInitializer
+        internal static FileExtensionToContentTypeDefinition vccSourceFileExtension = null;
+// ReSharper restore RedundantDefaultFieldInitializer
+
+        [Export]
+        [FileExtension(".h")]
+        [ContentType("vcc")]
+// ReSharper disable RedundantDefaultFieldInitializer
+        internal static FileExtensionToContentTypeDefinition vccHeaderFileExtension = null;
+// ReSharper restore RedundantDefaultFieldInitializer
+
+        [Export]
+        [Name("vcc")]
+// ReSharper disable RedundantDefaultFieldInitializer
+        internal static ClassificationTypeDefinition vccClassificationDefinition = null;
+// ReSharper restore RedundantDefaultFieldInitializer
+
+        [Export]
+        [Name("vcc.spec")]
+        [BaseDefinition("vcc")]
+// ReSharper disable RedundantDefaultFieldInitializer
+        internal static ClassificationTypeDefinition vccSpecDefinition = null;
+// ReSharper restore RedundantDefaultFieldInitializer
+    }
+}
