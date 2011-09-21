df7dcc7267424d54533d3a4e13fea515a5c78c1f
stobies <stobies@localhost>
2011-09-21 11:08:01 +0200

    Fixed spurious reporting of unlocalized smoke test errors Report trace with smoke test errors Added token information for Boogie.If to enable better smoke traces

---
+++ vcc/Test/scripts/add-testcase.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+opt=
+
+case "$2" in
+  vcc3/* )
+  opt=-3
+esac
+
+case "$2" in
+  vcc2/* | vcc2ns/* | vcc3/* )
+    case "$1" in
+      *.c )
+        chmod 644 "$2"
+	if grep -q ' _(' "$1" || grep -q '^_(' "$1" ; then
+	  echo '`/newsyntax' >> "$2"
+	fi
+        cat "$1" >> "$2"
+        echo '`' >> "$2"
+        echo '`' >> "$2"
+	sleep 1
+	perl -p -i -e 's/\r//g' "$2"
+	sleep 1
+	perl -p -i -e 's/\n/\r\n/g' "$2"
+	rm -f "$2".bak
+        ./fix $opt "$2"
+        ;;
+    esac
+esac
