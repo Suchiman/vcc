e4557b3c7bf6b16cb84891a61bb9cf80331ec083
stobies <stobies@localhost>
2011-09-21 14:37:42 +0200

    Fixed expected testcase output

--- vcc/Test/testsuite/vacid-0/SparseArray.c
+++ vcc/Test/testsuite/vacid-0/SparseArray.c
@@ -131,6 +131,14 @@ Verification of set succeeded.
 Verification of get succeeded.
 Verification of sparseArrayTestHarness succeeded.
 testcase(118,16) : warning : found unreachable code, possible soundness violation, please check the axioms or add an explicit assert(false)
+testcase(115,16) : warning : trace to unreachable location
+testcase(115,35) : warning : trace to unreachable location
+testcase(117,16) : warning : trace to unreachable location
+testcase(117,35) : warning : trace to unreachable location
+testcase(118,16) : warning : trace to unreachable location
 testcase(117,16) : warning : found unreachable code, possible soundness violation, please check the axioms or add an explicit assert(false)
+testcase(115,16) : warning : trace to unreachable location
+testcase(115,35) : warning : trace to unreachable location
+testcase(117,16) : warning : trace to unreachable location
 testcase(115,16) : warning : found unreachable code, possible soundness violation, please check the axioms or add an explicit assert(false)
 `*/
