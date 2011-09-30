978328d41f4f3407d86117bcbe7e72c7b00f1c09
Michal Moskal <michal@moskal.me>
2011-09-30 16:07:04 -0700

    Allow comparison of \object pointers

--- vcc/CodeModel2Vcc/normalizer.fs
+++ vcc/CodeModel2Vcc/normalizer.fs
@@ -97,6 +97,10 @@ namespace Microsoft.Research.Vcc
               if t1 <> t2 then
                 helper.Warning (c.Token, 9124, "pointers of different types (" + t1.ToString() + " and " + t2.ToString() + ") are never equal in pure context")
               Some (Expr.Macro (c, name + "_pure", [self p1; self p2]))
+            | ObjectT, Ptr(_)
+            | Ptr(_), ObjectT
+            | ObjectT, ObjectT ->
+              Some (Expr.Macro (c, name + "_pure", [self p1; self p2]))
             | _ ->
               helper.Oops (p1.Token, "cannot compare non-pointers as pointers: " + p1.Type.ToString() + " == " + p2.Type.ToString())
               None
