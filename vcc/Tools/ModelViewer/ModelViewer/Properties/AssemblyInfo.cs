6219e0730d6808a5bd6e01c7bf3dccc774ff1d97
MarkHillebrand <MarkHillebrand@localhost>
2011-10-24 21:07:08 +0200

    New Z3 version supports multiple hints
    Work item: 6423

---
+++ vcc/Tools/ModelViewer/ModelViewer/Properties/AssemblyInfo.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("VccModelViewer")]
+[assembly: AssemblyDescription("Visualisation of VCC error models")]
+#if DEBUG
+[assembly: AssemblyConfiguration("Debug")]
+#else
+[assembly: AssemblyConfiguration("Release")]
+#endif
+[assembly: AssemblyProduct("VccModelViewer")]
+[assembly: AssemblyTrademark("Microsoft Research Vcc")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
