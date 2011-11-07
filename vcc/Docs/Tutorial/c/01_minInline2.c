be72a28367b927464ea33885e5becd0a2d529622
Michal Moskal <michal@moskal.me>
2011-11-07 14:11:17 -0800

    Start the termination/call cycle checking

---
+++ vcc/Docs/Tutorial/c/01_minInline2.c
@@ -0,0 +1,16 @@
+//`/newsyntax
+/*{begin}*/
+#include <vcc.h>
+
+int main()
+{
+  int x,y,z;
+  if (x <= y)
+    z = x;
+  else z = y;
+  _(assert z <= x)
+  return 0;
+}
+/*`
+Verification of main succeeded.
+`*/
