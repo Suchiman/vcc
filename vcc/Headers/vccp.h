31bf4b870f6bc8ac69d75d5b5d71221ab6e7652b
Michal Moskal <michal@moskal.me>
2011-11-08 22:02:56 -0800

    Add \may_diverge() - require it to disable decreases checks

--- vcc/Headers/vccp.h
+++ vcc/Headers/vccp.h
@@ -145,6 +145,7 @@ _(\integer _(_boogie0) \index_within(\object,\object))
 _(\integer _(_boogie0) \sizeof_object(\object))
 _(bool _(_boogie0) \in_range_phys_ptr(\object))
 _(bool _(_boogie0) \in_range_spec_ptr(\object))
+_(bool _(_boogie0) \may_diverge())
 
 _(template<typename T> \integer \size(T dt);)
 
