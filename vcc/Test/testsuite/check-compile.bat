aec60baf09781081c43ff30f73d11b2ff4d87b2c
stobies <stobies@localhost>
2012-01-20 17:36:42 +0100

    Creating branch for C++ work

---
+++ vcc/Test/testsuite/check-compile.bat
@@ -0,0 +1,10 @@
+@REM echo off
+setlocal
+set HEADERS=%~dp0\..\..\Headers
+for /r . %%C in (*.c) do (
+  echo Compiling %%C
+  cd /d %%~dpC
+  cl.exe /c /I%HEADERS% %%~nxC
+  if errorlevel 1 echo ERROR&exit /b 1
+  del %%~nC.obj
+)
