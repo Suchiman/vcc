810b7886a058a9415858a9b811f6bddb81e4006b
stobies <stobies@localhost>
2012-01-20 14:21:37 +0100

    Remove non-3 projects and code Start clean-up of installer

--- vcc/Headers/vcc2.h
+++
@@ -1,9 +0,0 @@
-//-----------------------------------------------------------------------------
-//
-// Copyright (C) Microsoft Corporation.  All Rights Reserved.
-//
-//-----------------------------------------------------------------------------
-
-// Helper file to help in name transition from vcc2 to vcc
-
-#include <vcc.h>
