8c77549c3deab7420547f90d3bcb955f8a3d4a5a
MarkHillebrand <MarkHillebrand@localhost>
2012-01-23 11:38:09 +0100

    vcc\Docs\Tutorial: remove "/3" from VCC invocations

---
+++ vcc/Test/testsuite/examples/ClaimOnGroup.c
@@ -0,0 +1,28 @@
+#include <vcc.h>
+
+struct F {
+  int value;
+
+  _(group  _(claimable) g)
+
+  _(:g)
+  volatile int vol;
+
+  _(invariant \mine(\this::g))
+};
+
+int test(struct F *f)
+  _(requires \wrapped(f::g))
+  _(writes f::g)
+{
+  _(ghost \claim c)
+
+  _(assert ((struct F::g*)f)->\closed)
+
+  _(ghost c = \make_claim({f::g}, (f::g)->\closed))
+}
+
+/*`
+Verification of F#adm succeeded.
+Verification of test succeeded.
+`*/
