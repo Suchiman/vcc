3ca7db0e384d3bd7822bd85a5032d41678bd5c0b
stobies <stobies@localhost>
2012-01-24 10:50:17 +0100

    Taking care of some Resharper warnings

--- vcc/CppDriver/TransOptions.cs
+++ vcc/CppDriver/TransOptions.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data;
-using System.Linq;
-using System.Text;
+﻿using System.Collections.Generic;
 
 namespace Microsoft.Research.Vcc.Cpp
 {
