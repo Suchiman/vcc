1fb0cdbd8f5dd25904b7c1ff87b5957c7e77c786
dseifert <dseifert@SEIFERT02.europe.corp.microsoft.com>
2012-01-27 11:47:28 +0100

    Merge

---
+++ vcc/Transformers/MsCpp.fs
@@ -0,0 +1,34 @@
+﻿
+//-----------------------------------------------------------------------------
+//
+// Copyright (C) Microsoft Corporation.  All Rights Reserved.
+//
+//-----------------------------------------------------------------------------
+
+#light
+
+
+namespace Microsoft.Research.Vcc
+ open Microsoft.Research.Vcc
+ open Microsoft.Research.Vcc.Util
+ open Microsoft.Research.Vcc.TransUtil
+ open Microsoft.Research.Vcc.CAST
+ 
+ module MsCpp =
+  
+  // ============================================================================================================    
+
+  let rewriteExtraMacros self = function
+    | Macro(ec, "init", [lhs; rhs]) -> Some(Macro(ec, "=", [self lhs; self rhs]))
+    | _ -> None
+    
+
+  let init (helper:TransHelper.TransEnv) =
+
+    helper.AddTransformer ("cpp-begin", TransHelper.DoNothing)
+
+    helper.AddTransformer ("cpp-rewrite-macros", TransHelper.Expr rewriteExtraMacros)
+
+    helper.AddTransformer ("cpp-end", TransHelper.DoNothing)
+
+
