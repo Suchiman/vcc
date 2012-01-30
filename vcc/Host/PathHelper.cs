44d705677ce6f28963d600602ec5ad23b90810c2
stobies <stobies@localhost>
2012-01-30 16:31:55 +0100

    Fixed parsing of yarra argument Cleaned-up path searching code

--- vcc/Host/PathHelper.cs
+++ vcc/Host/PathHelper.cs
@@ -4,79 +4,63 @@
 //
 //-----------------------------------------------------------------------------
 
+using System;
 using System.IO;
 
 namespace Microsoft.Research.Vcc
 {
   static class PathHelper
   {
-    public static string Quote(string path)
+    private static string Quote(string path)
     {
-      if (path.Contains(" "))
-        return "\"" + path + "\"";
-      else
-        return path;
+      return path.Contains(" ") ? "\"" + path + "\"" : path;
     }
+
+    private static readonly Lazy<DirectoryInfo> cachedVccHeaderDirectory = new Lazy<DirectoryInfo>(
+      () =>
+        {
+          var dir = BinariesDirectory;
+          while (dir != null && dir.Exists)
+          {
+            foreach (var subdir in dir.GetDirectories("headers"))
+            {
+              if (subdir.GetFiles("vcc.h").Length > 0)
+                return subdir;
+            }
+            dir = dir.Parent;
+          }
+          return null;
+        }
+      );
 
     public static string/*?*/ GetVccHeaderDir(bool quoteResult) {
-      if (cachedVccHeaderDirectory == null) {
-        var dir = new FileInfo(typeof(PathHelper).Assembly.Location).Directory;
-        while (dir != null && dir.Exists) {
-          if (dir.GetFiles("vcc.h").Length > 0) {
-            cachedVccHeaderDirectory = dir;
-            goto terminateSearch;
-          }
-          foreach (DirectoryInfo subdir in dir.GetDirectories()) {
-            if (string.Compare(subdir.Name, "Headers", true) == 0 && subdir.GetFiles("vcc.h").Length > 0) {
-              cachedVccHeaderDirectory = subdir;
-              goto terminateSearch;
-            }
-          }
-          dir = dir.Parent;
-        }
-      }
 
-    terminateSearch:
+      if (cachedVccHeaderDirectory.Value == null) return null;
 
-      if (cachedVccHeaderDirectory == null) return null;
       if (quoteResult)
-        return Quote(cachedVccHeaderDirectory.FullName);
-      else
-        return cachedVccHeaderDirectory.FullName;
+        return Quote(cachedVccHeaderDirectory.Value.FullName);
+      
+      return cachedVccHeaderDirectory.Value.FullName;
     }
 
-    private static string BinariesDirectory
+    private static DirectoryInfo BinariesDirectory
     {
-      get
-      {
-        var dir = new FileInfo(typeof(PathHelper).Assembly.Location).Directory;
-        return dir.FullName;
-      }
+      get { return new FileInfo(typeof(PathHelper).Assembly.Location).Directory; }
     }
 
     public static string InspectorOption
     {
       get
       {        
-        return "/proverOpt:INSPECTOR=" + Quote(Path.Combine(BinariesDirectory, "Z3Inspector.exe"));
+        return "/proverOpt:INSPECTOR=" + Quote(Path.Combine(BinariesDirectory.FullName, "Z3Inspector.exe"));
       }
     }
-
-    public static string ModelViewerPath
-    {
-      get
-      {
-        return Path.Combine(BinariesDirectory, "VccModelViewer.exe");
-      }
-    }
-
-    private static DirectoryInfo cachedVccHeaderDirectory;
 
     public static string/*?*/ PluginDir {
       get {
-        PathHelper.GetVccHeaderDir(false);
-        if (cachedVccHeaderDirectory == null || cachedVccHeaderDirectory.Parent == null) return null;
-        DirectoryInfo[] candidates = cachedVccHeaderDirectory.Parent.GetDirectories("Plugins");
+        GetVccHeaderDir(false);
+        if (cachedVccHeaderDirectory == null || cachedVccHeaderDirectory.Value.Parent == null) return null;
+        DirectoryInfo[] candidates = cachedVccHeaderDirectory.Value.Parent.GetDirectories("Plugins");
         if (candidates.Length > 0) return candidates[0].FullName;
         return null;
       }
@@ -84,13 +68,12 @@ namespace Microsoft.Research.Vcc
 
     public static string PreludePath(string basename)
     {
-      if (basename.IndexOf(System.IO.Path.DirectorySeparatorChar) >= 0)
+      if (basename.IndexOf(Path.DirectorySeparatorChar) >= 0)
         return basename;
-      else {
-        string headersDir = PathHelper.GetVccHeaderDir(false);
-        if (headersDir != null) return System.IO.Path.Combine(headersDir, basename);
-        return null;
-      }
+      
+      string headersDir = GetVccHeaderDir(false);
+      if (headersDir != null) return Path.Combine(headersDir, basename);
+      return null;
     }
   }
 }
