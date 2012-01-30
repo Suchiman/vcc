75993405384a8774c4e443145b2ec03619648b6c
stobies <stobies@localhost>
2012-01-30 13:33:57 +0100

    Added transformer to perform conversion to bool where this is expected by the later transformers

---
+++ vcc/CppDriver/PathHelper.cs
@@ -0,0 +1,96 @@
+﻿//-----------------------------------------------------------------------------
+//
+// Copyright (C) Microsoft Corporation.  All Rights Reserved.
+//
+//-----------------------------------------------------------------------------
+
+using System.IO;
+
+namespace Microsoft.Research.Vcc
+{
+  static class PathHelper
+  {
+    public static string Quote(string path)
+    {
+      if (path.Contains(" "))
+        return "\"" + path + "\"";
+      else
+        return path;
+    }
+
+    public static string/*?*/ GetVccHeaderDir(bool quoteResult) {
+      if (cachedVccHeaderDirectory == null) {
+        var dir = new FileInfo(typeof(PathHelper).Assembly.Location).Directory;
+        while (dir != null && dir.Exists) {
+          if (dir.GetFiles("vcc.h").Length > 0) {
+            cachedVccHeaderDirectory = dir;
+            goto terminateSearch;
+          }
+          foreach (DirectoryInfo subdir in dir.GetDirectories()) {
+            if (string.Compare(subdir.Name, "Headers", true) == 0 && subdir.GetFiles("vcc.h").Length > 0) {
+              cachedVccHeaderDirectory = subdir;
+              goto terminateSearch;
+            }
+          }
+          dir = dir.Parent;
+        }
+      }
+
+    terminateSearch:
+
+      if (cachedVccHeaderDirectory == null) return null;
+      if (quoteResult)
+        return Quote(cachedVccHeaderDirectory.FullName);
+      else
+        return cachedVccHeaderDirectory.FullName;
+    }
+
+    private static string BinariesDirectory
+    {
+      get
+      {
+        var dir = new FileInfo(typeof(PathHelper).Assembly.Location).Directory;
+        return dir.FullName;
+      }
+    }
+
+    public static string InspectorOption
+    {
+      get
+      {        
+        return "/proverOpt:INSPECTOR=" + Quote(Path.Combine(BinariesDirectory, "Z3Inspector.exe"));
+      }
+    }
+
+    public static string ModelViewerPath
+    {
+      get
+      {
+        return Path.Combine(BinariesDirectory, "VccModelViewer.exe");
+      }
+    }
+
+    private static DirectoryInfo cachedVccHeaderDirectory;
+
+    public static string/*?*/ PluginDir {
+      get {
+        PathHelper.GetVccHeaderDir(false);
+        if (cachedVccHeaderDirectory == null || cachedVccHeaderDirectory.Parent == null) return null;
+        DirectoryInfo[] candidates = cachedVccHeaderDirectory.Parent.GetDirectories("Plugins");
+        if (candidates.Length > 0) return candidates[0].FullName;
+        return null;
+      }
+    }
+
+    public static string PreludePath(string basename)
+    {
+      if (basename.IndexOf(System.IO.Path.DirectorySeparatorChar) >= 0)
+        return basename;
+      else {
+        string headersDir = PathHelper.GetVccHeaderDir(false);
+        if (headersDir != null) return System.IO.Path.Combine(headersDir, basename);
+        return null;
+      }
+    }
+  }
+}
---
+++ vcc/Transformers/MsCpp.fs
@@ -0,0 +1,72 @@
+﻿
+//-----------------------------------------------------------------------------
+//
+// Copyright (C) Microsoft Corporation.  All Rights Reserved.
+//
+//-----------------------------------------------------------------------------
+
+#light
+
+
+namespace Microsoft.Research.Vcc
+ open Microsoft.Research.Vcc
+ open Microsoft.Research.Vcc.Util
+ open Microsoft.Research.Vcc.TransUtil
+ open Microsoft.Research.Vcc.CAST
+ 
+ module MsCpp =
+  
+  // ============================================================================================================    
+
+  let init (helper:TransHelper.TransEnv) =
+
+    // ============================================================================================================    
+
+    let rewriteExtraMacros self = 
+
+      // TODO: handle situations where the location incremented involves a func call, which should not be duplicated
+      // this is also wrong in CCI at the moment 
+      let handlePrePostIncrDecr e op isPost =
+        let (init, tmp) = cache helper "incdec" e VarKind.Local
+        let calc = Expr.Prim(e.Common, Op(op, CheckedStatus.Checked), [tmp; IntLiteral(e.Common, one)])
+        let assign = Macro(e.Common, "=", [e; calc])
+        if isPost then Expr.MkBlock(init @ [assign]) else Expr.MkBlock(init @ [assign; tmp])
+
+      function
+        | Macro(ec, "init", [lhs; rhs]) -> Some(Macro(ec, "=", [self lhs; self rhs]))
+        | Macro(_, "()++", [e]) -> Some(handlePrePostIncrDecr e "+" true)
+        | Macro(_, "()--", [e]) -> Some(handlePrePostIncrDecr e "-" true)
+        | Macro(_, "++()", [e]) -> Some(handlePrePostIncrDecr e "+" false)
+        | Macro(_, "--()", [e]) -> Some(handlePrePostIncrDecr e "-" false)
+
+        | _ -> None
+  
+    // ============================================================================================================    
+    
+    let insertBoolConversion self = 
+
+      // insert conversion to bool where this is expected later
+
+      let toBool (expr:Expr) = 
+        match expr.Type with
+          | Type.Bool -> self expr
+          | _ -> Cast({expr.Common with Type = Type.Bool}, CheckedStatus.Unchecked, self expr)
+
+      function
+        | Macro(ec, "for", [contr; inits; cond; inc; body]) ->
+          Some(Macro(ec, "for", [self contr; self inits; toBool cond; self inc; self body]))
+
+        | If(ec, tc, cond, th, el) -> 
+          Some(If(ec, tc, toBool cond, self th, self el))
+
+        | _ -> None
+          
+
+    // ============================================================================================================    
+
+    helper.AddTransformer ("cpp-begin", TransHelper.DoNothing)
+
+    helper.AddTransformer ("cpp-rewrite-macros", TransHelper.Expr rewriteExtraMacros)
+    helper.AddTransformer ("cpp-bool-conversion", TransHelper.Expr insertBoolConversion)
+
+    helper.AddTransformer ("cpp-end", TransHelper.DoNothing)
