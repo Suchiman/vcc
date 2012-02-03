94fdcf97a6c232edf585740a41f204a6d5bb5731
dseifert <dseifert@SEIFERT02.europe.corp.microsoft.com>
2012-02-03 13:45:00 +0100

    reverted temporarily disabling of global variable declaration

---
+++ vcc/Headers/vccpp.h
@@ -0,0 +1,22 @@
+//-----------------------------------------------------------------------------
+//
+// Copyright (C) Microsoft Corporation.  All Rights Reserved.
+//
+//-----------------------------------------------------------------------------
+
+#pragma once
+
+#ifndef VERIFY
+
+// hide annotations from C compiler
+
+#define _(...) /* nothing */
+
+#endif 
+
+namespace VCC
+{
+    void Assert(bool);
+    void Assume(bool);
+    bool Implies;
+}
