35c26e14fc1486e1f699b79f7679f42034952ba2
renehu <renehu@renehu4.europe.corp.microsoft.com>
2012-02-03 14:16:39 +0100

    Added ghost handling.

--- vcc/Headers/vccpp.h
+++ vcc/Headers/vccpp.h
@@ -6,17 +6,33 @@
 
 #pragma once
 
-#ifndef VERIFY
-
-// hide annotations from C compiler
-
-#define _(...) /* nothing */
-
-#endif 
-
 namespace VCC
 {
     void Assert(bool);
     void Assume(bool);
-    //bool Implies;
+    void BeginGhost();
+    void EndGhost();
+//    bool Implies;
 }
+
+#ifndef VERIFY
+
+// hide annotations from C compiler
+
+#define _(...)  /* nothing */
+
+#else
+
+#define REWRITE_GHOST
+
+#endif 
+
+#ifdef REWRITE_GHOST
+
+#define vcc_ghost(X) VCC::BeginGhost(); X; VCC::EndGhost();
+
+#else
+
+#define vcc_ghost(...) /* nothing */
+
+#endif
