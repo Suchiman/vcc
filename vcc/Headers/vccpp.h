0189ad4add2a5489b9375e71d6fc351a52d99ba7
renehu <renehu@renehu2.europe.corp.microsoft.com>
2012-02-08 10:44:59 +0100

    Adapted to rewriter change.

--- vcc/Headers/vccpp.h
+++ vcc/Headers/vccpp.h
@@ -6,6 +6,14 @@
 
 #pragma once
 
+#ifndef VERIFY
+
+// hide annotations from C compiler
+
+#define _(...) /* nothing */
+
+#else
+
 namespace VCC
 {
     void Assert(bool);
@@ -17,24 +25,4 @@ namespace VCC
     template<class T> bool Exists(T);
 }
 
-#ifndef VERIFY
-
-// hide annotations from C compiler
-
-#define _(...) /* nothing */
-
-#else
-
-#define REWRITE_GHOST
-
 #endif 
-
-#ifdef REWRITE_GHOST
-
-#define vcc_ghost(X) VCC::BeginGhost(); X; VCC::EndGhost();
-
-#else
-
-#define vcc_ghost(...) /* nothing */
-
-#endif
