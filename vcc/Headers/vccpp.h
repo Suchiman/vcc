f08dc4c54529095feebb9bf7877df54133d6f642
renehu <renehu@renehu2.europe.corp.microsoft.com>
2012-02-14 12:28:26 +0100

    Added support for \claim and \make_claim.

--- vcc/Headers/vccpp.h
+++ vcc/Headers/vccpp.h
@@ -18,6 +18,9 @@
 
 namespace VCC
 {
+    // types
+    typedef int Claim;
+
     // assert/assume
     void Assert(bool);
     void Assume(bool);
@@ -40,25 +43,26 @@ namespace VCC
     void VolatileOwns();
     
     // object state
-    template<class T> bool Activeclaim(T)   { return true; };
-    template<class T> void Always(T, bool)  { };
-    template<class T> void Atomic(T)        { };
-    template<class T> bool Claims(T, bool)  { return true; };
-    template<class T> int Claimcount(T)     { return 0; };
-    template<class T> bool Closed(T)        { return true; };
-    template<class T> bool Consistent(T)    { return true; };
-    template<class T> void HavocOthers(T)   { };
-    template<class T> bool Mine(T)          { return true; };
-    template<class T> bool Mutable(T)       { return true; };
-    template<class T> T Old(T)              { T t; return t; };
-    template<class T> int Span(T)           { return 0; };
-    template<class T> bool Threadlocal(T)   { return true; };
-    template<class T> void Unwrapping(T)    { };
-    template<class T> bool Valid(T)         { return true; };
-    template<class T> bool Wrapped(T)       { return true; };
+    template<class T> bool Activeclaim(T)       { return true; };
+    template<class T> void Always(T, bool)      { };
+    template<class T> void Atomic(T)            { };
+    template<class T> bool Claims(T, bool)      { return true; };
+    template<class T> int Claimcount(T)         { return 0; };
+    template<class T> bool Closed(T)            { return true; };
+    template<class T> bool Consistent(T)        { return true; };
+    template<class T> void HavocOthers(T)       { };
+    template<class T> Claim Makeclaim(T, bool)  { };
+    template<class T> bool Mine(T)              { return true; };
+    template<class T> bool Mutable(T)           { return true; };
+    template<class T> T Old(T)                  { T t; return t; };
+    template<class T> int Span(T)               { return 0; };
+    template<class T> bool Threadlocal(T)       { return true; };
+    template<class T> void Unwrapping(T)        { };
+    template<class T> bool Valid(T)             { return true; };
+    template<class T> bool Wrapped(T)           { return true; };
     
-    template<class T> void* Owns(T)         { return 0; };
-    template<class T> void* Owner(T)        { return 0; };
+    template<class T> void* Owns(T)             { return 0; };
+    template<class T> void* Owner(T)            { return 0; };
    
     // statements
     template<class T> void Wrap(T o)
