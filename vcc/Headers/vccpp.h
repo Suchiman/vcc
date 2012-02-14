f003f73f38ad8c12998606d90c466e605ed03a02
stobies <stobies@localhost>
2012-02-14 09:29:06 +0100

    Adding bodies to the VCC functions so that they appear in the AST

--- vcc/Headers/vccpp.h
+++ vcc/Headers/vccpp.h
@@ -30,35 +30,35 @@ namespace VCC
     void Invariant(bool);
     void Pure();
     void Requires(bool);
-    template<class T> void Reads(T);
-    template<class T> T Result();
-    template<class T> void Returns(T);
-    template<class T> void Writes(T);
+    template<class T> void Reads(T)     { };
+    template<class T> T Result()        { T t; return t; };
+    template<class T> void Returns(T)   { };
+    template<class T> void Writes(T)    { };
 
     // data contracts
     void DynamicOwns();
     void VolatileOwns();
     
     // object state
-    template<class T> bool Activeclaim(T);
-    template<class T> void Always(T, bool);
-    template<class T> void Atomic(T);
-    template<class T> bool Claims(T, bool);
-    template<class T> int Claimcount(T);
-    template<class T> bool Closed(T);
-    template<class T> bool Consistent(T);
-    template<class T> void HavocOthers(T);
-    template<class T> bool Mine(T);    
-    template<class T> bool Mutable(T);
-    template<class T> T Old(T);
-    template<class T> int Span(T);
-    template<class T> bool Threadlocal(T);
-    template<class T> void Unwrapping(T);
-    template<class T> bool Valid(T);
-    template<class T> bool Wrapped(T);
+    template<class T> bool Activeclaim(T)   { return true; };
+    template<class T> void Always(T, bool)  { };
+    template<class T> void Atomic(T)        { };
+    template<class T> bool Claims(T, bool)  { return true; };
+    template<class T> int Claimcount(T)     { return 0; };
+    template<class T> bool Closed(T)        { return true; };
+    template<class T> bool Consistent(T)    { return true; };
+    template<class T> void HavocOthers(T)   { };
+    template<class T> bool Mine(T)          { return true; };
+    template<class T> bool Mutable(T)       { return true; };
+    template<class T> T Old(T)              { T t; return t; };
+    template<class T> int Span(T)           { return 0; };
+    template<class T> bool Threadlocal(T)   { return true; };
+    template<class T> void Unwrapping(T)    { };
+    template<class T> bool Valid(T)         { return true; };
+    template<class T> bool Wrapped(T)       { return true; };
     
-    template<class T> void* Owns(T);
-    template<class T> void* Owner(T);
+    template<class T> void* Owns(T)         { return 0; };
+    template<class T> void* Owner(T)        { return 0; };
    
     // statements
     template<class T> void Wrap(T o)
@@ -75,6 +75,6 @@ namespace VCC
     bool Implies;
 
     // templates for quantifiers
-    template<class T> bool ForAll(T);
-    template<class T> bool Exists(T);
+    template<class T> bool ForAll(T)  { return true; };
+    template<class T> bool Exists(T)  { return true; };
 }
