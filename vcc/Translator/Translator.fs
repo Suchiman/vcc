cce0af5ae1c45c7fb8f559ad76eb928d59b96dcc
stobies <stobies@localhost>
2012-02-24 07:27:59 +0100

    Do not crash for null weights

--- vcc/Translator/Translator.fs
+++ vcc/Translator/Translator.fs
@@ -528,7 +528,7 @@ namespace Microsoft.Research.Vcc
                   bTrue
                 else
                   let (body, triggers) = TriggerInference(helper, preludeBodies, c.Token, invMapping, vars).Run (body, List.map selfs q.Triggers)
-                  let weight n = if q.Weight = "" then weight n else weight q.Weight
+                  let weight n = if System.String.IsNullOrEmpty(q.Weight) then weight n else weight q.Weight
                   match q.Kind with
                     | C.Forall -> B.Forall (c.Token, vars, triggers, weight "user-forall", body)
                     | C.Exists -> B.Exists (c.Token, vars, triggers, weight "user-exists", body)
