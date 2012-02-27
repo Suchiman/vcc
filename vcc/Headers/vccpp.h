59c4b2e4741370cb1d4f859c6994c5a6f7dc53ae
renehu <renehu@renehu2.europe.corp.microsoft.com>
2012-02-27 11:27:43 +0100

    Added support for inv.

--- vcc/Headers/vccpp.h
+++ vcc/Headers/vccpp.h
@@ -87,6 +87,7 @@ namespace VCC
     template<class T> bool Fresh(T)                     { return true; };
     template<class T> void HavocOthers(T)               { };
     template<class T> bool In(T, Set)                   { return true; }; 
+    template<class T> bool Inv(T)                       { return true; };
     template<class T> Claim Makeclaim(T, bool)          { };
     template<class T> bool Mallocroot(T)                { return true; }
     template<class T> bool Mine(T, ...)                 { return true; };
