15c5a06a0413298d149ae1793faa5ef17529350b
stobies <stobies@localhost>
2012-03-01 16:34:43 +0100

    Arrayrange has second argument of type size_t Deal with loop invariants on do-while and while loops Better error reporting for illegal implicit casts

---
+++ vcc/Headers/vcc.h
@@ -0,0 +1,31 @@
+//-----------------------------------------------------------------------------
+//
+// Copyright (C) Microsoft Corporation.  All Rights Reserved.
+//
+//-----------------------------------------------------------------------------
+#ifndef _VCC_H
+#define _VCC_H
+
+#ifndef VERIFY
+
+#define _(...) /* VCC annotations hidden from compiler */
+
+#else
+
+#ifndef __cplusplus
+
+#include <crtdefs.h>
+
+#include <vccp.h>
+
+#endif
+
+#endif
+
+#ifdef __cplusplus
+
+#include <vccpp.h>
+
+#endif
+
+#endif // _VCC_H
