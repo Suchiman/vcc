34dccfe8270d8ce7a2eca777926bac42d8c397bc
renehu <renehu@renehu4.europe.corp.microsoft.com>
2012-03-02 16:18:16 +0100

    Added assignment operator to Object class.

---
+++ vcc/Headers/vccpp.h
@@ -0,0 +1,221 @@
+//-----------------------------------------------------------------------------
+//
+// Copyright (C) Microsoft Corporation.  All Rights Reserved.
+//
+//-----------------------------------------------------------------------------
+
+#pragma once
+
+#include <vccpp-m.h>
+
+#ifndef VERIFY
+
+// hide annotations from C compiler
+
+#define _(...) /* nothing */
+
+#endif 
+
+namespace VCC
+{
+    // types
+    template <typename T> class Ghost
+    {
+    private:
+      T _t_member_;
+
+    public:
+      Ghost(T t);
+      Ghost(const Ghost<T> &g);
+      operator T() const;
+    };
+
+    class Claim {
+    public:
+      Claim();
+      Claim(const Claim&);
+      Claim(const volatile Claim&);
+      Claim operator=(const Ghost<Claim>&);
+      Claim operator=(const volatile Claim&);
+      bool operator==(Claim);
+      bool operator!=(Claim);
+    };
+
+    class Integer {
+    public:
+      bool operator==(Integer);
+      bool operator!=(Integer);
+      bool operator<(Integer);
+      bool operator>(Integer);
+      bool operator<=(Integer);
+      bool operator>=(Integer);
+    };
+
+    class Natural {
+    public:
+      bool operator==(Natural);
+      bool operator!=(Natural);
+      bool operator<(Natural);
+      bool operator>(Natural);
+      bool operator<=(Natural);
+      bool operator>=(Natural);
+    };
+
+    class Object {
+    public:
+      bool operator==(void*);
+      bool operator!=(void*);
+      Object operator=(void*);
+    };
+    
+    class Set {
+    public:
+      Set(...);
+      bool operator==(Set);
+      bool operator!=(Set);
+      bool operator-=(Set);
+      bool operator+=(Set);
+    };
+
+    class State {      
+    public:
+      State();
+      State(const volatile State&);
+      State(const State&);
+      bool operator==(State);
+      bool operator!=(State);
+    };
+
+    template <class From, class To> class Map {
+    public:
+      To& operator[](From);
+      To& operator[](From) volatile;
+      bool operator==(Map<From,To>);
+      volatile Map<From, To> operator=(const Map<From, To>&) volatile;
+    };
+
+    // assert/assume
+    void Assert(bool);
+    void Assume(bool);
+
+    // method contracts
+    void Admissibility();
+    void AtomicInline();
+    void BeginUpdate();
+    void Ensures(bool);
+    void FrameAxiom();
+    void Invariant(bool);
+    void Pure();
+    void Requires(bool);
+    template<class T> void ContractFor(T);
+    void Reads(...);
+    template<class T> T Result();
+    template<class T> void Returns(T);
+    void Writes(...);
+
+    // data contracts
+    void DynamicOwns();
+    void VolatileOwns();
+    
+    // object state
+    template<class T> bool Activeclaim(T);
+    template<class T1, class T2> bool Addreq(T1, T2);
+    template<class T> void Always(T, bool);
+    template<class T> T* Alloc();
+    template<class T1, class T2> bool Approves(T1, T2);
+    template<class T> T Array(T t, size_t);
+    template<class T> Set Arrayrange(T t, size_t);
+    template<class T> bool Arraysdisjoint(T, size_t, T, size_t);
+    template<class T> T At(State, T t);
+    void Atomic(...);
+    template<class T> void BumpVolatileVersion(T);
+    bool Claims(Claim, bool);
+    template<class T> int Claimcount(T);
+    template<class T> bool Claimsobject(Claim, T);
+    template<class T> bool Closed(T);
+    template<class T1, class T2> bool Depends(T1, T2);
+    template<class T> void Destroyclaim(Claim, T);
+    Set Diff(Set s, Set);
+    template<class T> Set Domain(T);
+    template<class T> Set Extent(T t);
+    template<class T> bool Extentmutable(T t);
+    template<class T> bool Fresh(T);
+    bool Fullcontext();
+    template<class T> void HavocOthers(T);
+    template<class T> bool In(T, Set);
+    template<class T> bool In0(T, Set);
+    template<class T> bool Inv(T);
+    template<class T, class V> bool Is(V);
+    template<class T> T Labeled(const char*, T t);
+    template<class T1, class T2, class L> Map<T1, T2> Lambda(L);
+    template<class T> Claim Makeclaim(T, bool);
+    template<class T> bool Mallocroot(T);
+    template<class T> bool Matchulong(T);
+    bool Mine(...);
+    template<class T> bool Mutable(T);
+    template<class T> bool Mutablearray(T, int);
+    template<class T> bool Nonprimitiveptr(T);
+    template<class T> bool Notshared(T);
+    State Now();
+    template<class T> bool Objectroot(T);
+    template<class T> T Old(T);
+    template<class T> bool Onunwrap(T, bool);
+    bool Programentrypoint();
+    template<class T> int Span(T);
+    template<class T> bool Threadlocal(T);
+    template<class T> bool Threadlocalarray(T, int);
+    template<class T> bool Unchanged(T);
+    Set Union(Set s, Set);
+    Set Universe();
+    void Unwrapping(...);
+    template<class T> bool Valid(T);
+    template<class T> T Whenclaimed(T t);
+    template<class T> bool Wrapped(T);
+    template<class T> bool Wrapped0(T);
+    
+    template<class T> Set Owns(T);
+    template<class T> Object Owner(T);
+   
+    template <typename T> class GhostOut
+    {
+    private:
+      T _t_member_;
+
+    public:
+      GhostOut(T t);
+      operator T() const;
+    };
+    
+    template <typename T> class Unchecked 
+    {
+    private:
+      T _t_member_;
+
+    public:
+      Unchecked(const T&);
+      operator T() const;
+    };
+    
+    template <typename T> Ghost<T> CreateGhost(T);
+    template <typename T> GhostOut<T> CreateGhostOut(T);
+
+    // statements
+    template<class T> void Wrap(T o)
+    {
+      VCC::Writes(o); // TODO: also writes o->\owns
+    }
+
+    template<class T> void Unwrap(T o)
+    {
+      VCC::Writes(o);
+    }
+
+    // special variables
+    bool Equivalence;
+    bool Implies;
+
+    // templates for quantifiers
+    template<class T> bool ForAll(T);
+    template<class T> bool Exists(T);
+    void Trigger(...);
+}
---
+++ vcc/VSPackage/VCCLauncher.cs
@@ -0,0 +1,280 @@
+﻿namespace Microsoft.Research.Vcc.VSPackage
+{
+    using System;
+    using System.IO;
+    using System.Reflection;
+    using System.Text.RegularExpressions;
+    using System.Threading;
+    using System.Windows.Forms;
+    using Microsoft.Win32;
+
+    /// <summary>
+    ///     This class is used to start VCC.exe with the correct parameters and to get the results from VCC.exe
+    /// </summary>
+    internal static class VCCLauncher
+    {
+        private static NotifyIcon notifyIcon;
+
+        internal static string GetVccVersion()
+        {
+            Assembly assembly = Assembly.GetExecutingAssembly();
+            return assembly.GetName().Version.ToString();
+        }
+
+        private static string GetVSCOMNTOOLS()
+        {
+            string Version = VSIntegration.DTE.Version;      //returns something like 8.0
+            string CleanVersionTag = Version.Replace(".", "");
+            string VSDir = Environment.GetEnvironmentVariable(String.Format("VS{0}COMNTOOLS", CleanVersionTag));
+            return VSDir;
+        }
+
+        internal static string GetCLPath(string ActivePlatform)
+        {
+            string vsToolDir = GetVSCOMNTOOLS();
+            if (!String.IsNullOrEmpty(vsToolDir))
+            {
+                string CL = (ActivePlatform == "x64") ? "VC\\bin\\x86_amd64\\cl.exe" : "VC\\bin\\cl.exe";
+                return vsToolDir.ToUpperInvariant().Replace("COMMON7\\TOOLS\\", CL);
+            }
+            else return null;
+        }
+
+        #region commands
+
+        private static string GetArgumentsFromOptions(VccOptionPage options, bool respectCustomFlag)
+        {
+            string result = !respectCustomFlag || options.UseAdditionalCommandlineArguments ?
+                options.AdditionalCommandlineArguments :
+                string.Empty;
+
+            if (options.ShowZ3Inspector)
+            {
+                result += " /i";
+            }
+
+            result += " /bvd";
+
+            return result;
+        }
+
+        internal static void VerifyThis(string filename, string currentFile, int line, VccOptionPage options)
+        {
+            string addArguments = GetArgumentsFromOptions(options, true);
+            addArguments += String.Format(" /loc:\"{0}\":{1} ", currentFile, line);
+            LaunchVCC(filename, addArguments);
+        }
+
+        internal static void CustomVerify(string filename, VccOptionPage options)
+        {
+            using (var customVerifyForm = new CustomVerifyForm(GetArgumentsFromOptions(options, false)))
+            {
+                if (customVerifyForm.ShowDialog() == DialogResult.OK)
+                {
+                    LaunchVCC(filename, customVerifyForm.Arguments);
+                }
+            }
+        }
+
+        internal static void VerifyFile(string filename, VccOptionPage options)
+        {
+            string addArguments = GetArgumentsFromOptions(options, true);
+            LaunchVCC(filename, addArguments);
+        }
+
+        internal static void VerifyFileWithoutIncludes(string filename, string currentFile, VccOptionPage options)
+        {
+            string addArguments = GetArgumentsFromOptions(options, true);
+            addArguments += " /ii:" + currentFile;
+            LaunchVCC(filename, addArguments);
+        }
+
+        #endregion
+
+        #region process handling
+
+        /// <summary>
+        ///     This string contains the Path of the Vcc-Executable.
+        ///     User Input in Tools/Options/Vcc > Registry Entry > "vcc.exe"
+        /// </summary>
+        private static string VccPath
+        {
+            get
+            {
+                if (String.IsNullOrWhiteSpace(VSPackagePackage.Instance.OptionPage.VccExecutableFolder))
+                {
+                    using (var key = Registry.LocalMachine.OpenSubKey(@"Software\Microsoft Research\Vcc", false))
+                    {
+                        if (key != null)
+                        {
+                            return key.GetValue("vccExecutablePath", "vcc.exe") as string;
+                        }
+                        else
+                        {
+                            return "vcc.exe";
+                        }
+                    }
+                }
+                else
+                {
+                    return Path.Combine(VSPackagePackage.Instance.OptionPage.VccExecutableFolder, "vcc.exe");
+                }
+            }
+        }
+
+        internal static void LaunchVCC(string filename, string vccargs)
+        {
+            vccargs += " ";
+            vccargs += VSIntegration.CurrentCompilerSettings.ToVccOptions();
+            var clPath = GetCLPath(VSIntegration.CurrentPlatform);
+            if (clPath != null) vccargs += String.Format(" /clpath:\"{0}\"", clPath);
+
+            VSIntegration.ClearErrorsAndMarkers();
+            VSIntegration.UpdateStatus("Verifying...", true);
+
+            //// Clear Verification Outputpane
+            VSIntegration.ClearAndShowPane();
+            VSIntegration.WriteToPane("=== VCC started. ===");
+            //// Write Commandline-Command to Verification Outputpane
+            VSIntegration.WriteToPane(string.Format("Command Line: \"{0}\" {1}\n", VccPath, vccargs));
+            //// Get notified when VCC-Process finishes.
+
+            //// Start the process
+            try
+            {
+                VCCRunning = true;
+
+                // Get CL path //TODO generic clpath
+                string cl11Path = Path.Combine(Environment.CurrentDirectory, "..\\..\\..\\..\\..\\..\\..\\Ext\\VS11\\cl.exe");
+
+                // Subscribe to message events
+                VccppMain.VccppEvent += new EventHandler<VccppEventArgs>(vccProcess_OutputDataReceived);
+
+                // TODO vccargs
+                int exitCode = VccppMain.ProcessFile(filename, cl11Path, null, false);
+                vccProcess_Exited(exitCode);
+
+                //// When the process was started, remember the cmdlinearguments
+                VSPackagePackage.LastArguments = vccargs;
+                VSPackagePackage.LastFilename = filename;
+            }
+            catch (Exception)
+            {
+                VSIntegration.WriteToPane("Executing\n" + VccPath + "\nfailed.\n"
+                    + "You can specify the folder in which the VCC executable is located in the menu under Verify/Vcc Options/Vcc/General.");
+                VSIntegration.WriteToPane("=== Verification failed. ===");
+                VSIntegration.UpdateStatus("Verification failed.", false);
+            }
+        }
+
+        /// <summary>
+        ///     Cancels the running VCC Process, if it exists
+        /// </summary>
+        internal static void Cancel()
+        {
+            if (VCCRunning)
+            {
+                // vccProcess_Exited(-1); TODO
+            }
+        }
+
+        #endregion
+
+        #region process observation
+
+        //// This is set to true, when Verification fails.
+        private static readonly Regex VCCErrorRegEx =
+            new Regex(@"(?<path>(.*?))\(((?<line>([0-9]+))|(?<line>([0-9]+)),(?<column>([0-9]+)))\)\s:\s(((error\s(.*?):)\s(?<errormessage>(.*)))|(?<errormessage>\(Location of symbol related to previous error.\)))");
+        private static readonly Regex VCCWarningRegEx =
+            new Regex(@"(?<path>(.*?))\(((?<line>([0-9]+))|(?<line>([0-9]+)),(?<column>([0-9]+)))\)\s:(\swarning\s(.*?):)\s(?<errormessage>(.*))");
+
+        private static void vccProcess_Exited(int exitCode)
+        {
+            VCCRunning = false;
+
+            if (VSPackagePackage.Instance.OptionPage.ShowNotifications &&
+                new IntPtr(VSIntegration.DTE.MainWindow.HWnd) != NativeMethods.GetForegroundWindow())
+            {
+                if (notifyIcon == null)
+                {
+                    notifyIcon = new NotifyIcon();
+                    notifyIcon.Icon = VSPackage.Resources.VccIcon;
+                    notifyIcon.Visible = true;
+                    notifyIcon.Text = "Vcc " + GetVccVersion();
+                }
+
+                if (exitCode == 0)
+                {
+                    notifyIcon.ShowBalloonTip(4000, "Verification succeeded!", "Verification run completed successfully.", ToolTipIcon.Info);
+                }
+                else
+                {
+                    notifyIcon.ShowBalloonTip(4000, "Verification failed!", "Verification run completed with errors.", ToolTipIcon.Error);
+                }
+            }
+
+            switch (exitCode)
+            {
+                case -1:
+                    VSIntegration.WriteToPane("\n=== VCC was canceled. ===");
+                    VSIntegration.UpdateStatus("Verification canceled.", false);
+                    break;
+                case 0:
+                    Thread.Sleep(1000);
+                    VSIntegration.WriteToPane("\n=== Verification succeeded. ===");
+                    VSIntegration.UpdateStatus("Verification succeeded.", false);
+                    break;
+                case 2:
+                    Thread.Sleep(1000);
+                    VSIntegration.WriteToPane("Incorrect commandline arguments were used.");
+                    VSIntegration.WriteToPane("\n=== Verification failed. ===\n");
+                    VSIntegration.UpdateStatus("Verification failed.", false);
+                    break;
+                case 1:
+                case 3:
+                    Thread.Sleep(1000);
+                    VSIntegration.WriteToPane("\n=== Verification failed. ===");
+                    VSIntegration.UpdateStatus("Verification failed.", false);
+                    break;
+                default:
+                    Thread.Sleep(1000);
+                    VSIntegration.WriteToPane("\n=== VCC finished with unknown exitcode. ===");
+                    VSIntegration.WriteToPane(exitCode.ToString());
+                    break;
+            }
+        }
+
+        private static void vccProcess_OutputDataReceived(object sender, VccppEventArgs e)
+        {
+            //// Write Output from VCC to Verification Outputpane
+            if (e != null && !String.IsNullOrEmpty(e.Message))
+            {
+                VSIntegration.WriteToPane(e.Message);
+
+                Match match;
+                if ((match = VCCErrorRegEx.Match(e.Message)).Success)
+                {
+                    //// Add error to error list
+                    VSIntegration.AddErrorToErrorList(
+                      match.Groups["path"].Value,
+                      match.Groups["errormessage"].Value,
+                      Int32.Parse(match.Groups["line"].Value),
+                      Microsoft.VisualStudio.Shell.TaskErrorCategory.Error);
+                }
+                else if ((match = VCCWarningRegEx.Match(e.Message)).Success)
+                {
+                    //// Add warning to error list
+                    VSIntegration.AddErrorToErrorList(
+                      match.Groups["path"].Value,
+                      match.Groups["errormessage"].Value,
+                      Int32.Parse(match.Groups["line"].Value),
+                      Microsoft.VisualStudio.Shell.TaskErrorCategory.Warning);
+                }
+            }
+        }
+
+        internal static bool VCCRunning { get; private set; }
+
+        #endregion
+    }
+}
