cf662dc4e1ccd30b0d47ff08587d11ee50eadafd
renehu <renehu@renehu2.europe.corp.microsoft.com>
2012-03-07 16:46:51 +0100

    Added support for blob.

--- vcc/Headers/vccpp.h
+++ vcc/Headers/vccpp.h
@@ -183,6 +183,8 @@ namespace VCC
     template<class T> T At(State, T t);
     void Atomic(...);
     template<class T> void BeginGhostAtomic(T);
+    template<class T> Object Blob(T);
+    template<class T1, class T2> Object Blob(T1, T2);
     template<class T> void Blobify(T);    
     template<class T> void BumpVolatileVersion(T);
     bool Claims(Claim, bool);
