4e190bb18524b298aec7d6e07058c35b605eeed0
dseifert <dseifert@localhost>
2012-03-16 22:19:27 +0100

    removed type annotations from \result

--- vcc/Headers/vccpp.h
+++ vcc/Headers/vccpp.h
@@ -285,13 +285,13 @@ namespace VCC
 
     bool Matchulong(unsigned __int64) {
       _(pure)
-      _(ensures \result<bool> == true)
+      _(ensures \result == true)
       return true;
     };
 
     bool Matchlong(__int64) {
       _(pure)
-      _(ensures \result<bool> == true)
+      _(ensures \result == true)
       return true;
     };
 
