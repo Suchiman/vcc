f8062fdbb42f35806c69525b1bbbc222b4bcceaf
dinonu <dinonu@localhost>
2012-03-29 17:09:35 +0200

    Storing initial vcc args parsing effort, not finished, so far: - checks for supported switches - added VccppOptions which will beholding the logic for parsing additional VCC switches, on top of Boogie ones - parsing is now started from the CppDriver constructor instead of CppDriver.Verify()

--- vcc/CppDriver/TransOptions.cs
+++ vcc/CppDriver/TransOptions.cs
@@ -1,84 +1,86 @@
-﻿using System.Collections.Generic;
-
-namespace Microsoft.Research.Vcc.Cpp
+﻿namespace Microsoft.Research.Vcc.Cpp
 {
-  class TransOptions : Helper.Options
-  {
-    private readonly string[] pipeOperations;
+    using System.Collections.Generic;
 
-    public TransOptions(string[] pipeOperations)
+    class TransOptions : Helper.Options
     {
-      this.pipeOperations = pipeOperations; 
-    }
+        private readonly string[] pipeOperations;
+        private readonly VccppOptions vccppOptions;
 
-    public override bool AggressivePruning
-    {
-      get { return false; }
-    }
+        public TransOptions(string[] pipeOperations, VccppOptions vccppOptions)
+        {
+            this.vccppOptions = vccppOptions;
+            this.pipeOperations = pipeOperations;
+        }
 
-    public override int DefExpansionLevel
-    {
-      get { return 0; }
-    }
+        public override bool AggressivePruning
+        {
+            get { return false; }
+        }
 
-    public override bool DeterminizeOutput
-    {
-      get { return false; }
-    }
+        public override int DefExpansionLevel
+        {
+            get { return 0; }
+        }
 
-    public override int DumpTriggers
-    {
-      get { return 0; }
-    }
+        public override bool DeterminizeOutput
+        {
+            get { return false; }
+        }
 
-    public override bool ExplicitTargetsGiven
-    {
-      get { return false; }
-    }
+        public override int DumpTriggers
+        {
+            get { return 0; }
+        }
 
-    public override IEnumerable<string> Functions
-    {
-      get { return new string[] {}; }
-    }
+        public override bool ExplicitTargetsGiven
+        {
+            get { return false; }
+        }
 
-    public override bool OpsAsFunctions
-    {
-      get { return false; }
-    }
+        public override IEnumerable<string> Functions
+        {
+            get { return new string[] { }; }
+        }
 
-    public override IEnumerable<string> PipeOperations
-    {
-      get { return pipeOperations; }
-    }
+        public override bool OpsAsFunctions
+        {
+            get { return false; }
+        }
 
-    public override bool PrintCEVModel
-    {
-      get { return false; }
-    }
+        public override IEnumerable<string> PipeOperations
+        {
+            get { return pipeOperations; }
+        }
 
-    public override bool TerminationForAll
-    {
-      get { return false; }
-    }
+        public override bool PrintCEVModel
+        {
+            get { return false; }
+        }
 
-    public override bool TerminationForGhost
-    {
-      get { return false; }
-    }
+        public override bool TerminationForAll
+        {
+            get { return false; }
+        }
 
-    public override bool TerminationForPure
-    {
-      get { return false; }
-    }
+        public override bool TerminationForGhost
+        {
+            get { return false; }
+        }
 
-    public override IEnumerable<string> WeightOptions
-    {
-      get { return new string[] {}; }
-    }
+        public override bool TerminationForPure
+        {
+            get { return false; }
+        }
 
-    public override bool YarraMode
-    {
-      get { return false; }
+        public override IEnumerable<string> WeightOptions
+        {
+            get { return new string[] { }; }
+        }
+
+        public override bool YarraMode
+        {
+            get { return false; }
+        }
     }
-  }
 }
