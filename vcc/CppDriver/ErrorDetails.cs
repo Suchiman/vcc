c17e159ee1843cd1fb26b8c77972d12ea1c1768f
stobies <stobies@localhost>
2012-04-02 16:26:48 +0200

    Report related errors and consider them for output matching during test case runs

--- vcc/CppDriver/ErrorDetails.cs
+++ vcc/CppDriver/ErrorDetails.cs
@@ -8,7 +8,12 @@ namespace Microsoft.Research.Vcc
       new Regex(@"(?<path>(.*?))\((?<line> \d+),(?<column> \d+)\)\s*:\s+(?<kind> (error|warning))\s+\w\w (?<errno>\d+):(?<msg>.*)", 
           RegexOptions.Compiled | RegexOptions.Singleline | RegexOptions.IgnorePatternWhitespace);
 
-    public ErrorDetails(string filename, bool isWarning, int line, int column, int errno, string msg)
+    private static readonly Regex RelatedErrorPattern =
+      new Regex(@"(?<path>(.*?))\((?<line> \d+),(?<column> \d+)\)\s*:\s+(?<kind> (error|warning))\s+:\s+\(Location\ of\ symbol\ related\ to\ previous",
+          RegexOptions.Compiled | RegexOptions.Singleline | RegexOptions.IgnorePatternWhitespace);
+
+
+    public ErrorDetails(string filename, bool isWarning, int line, int column, int errno, string msg, bool isRelated)
     {
       this.Filename = filename;
       this.IsWarning = isWarning;
@@ -16,6 +21,7 @@ namespace Microsoft.Research.Vcc
       this.Column = column;
       this.ErrorNumber = errno;
       this.Message = msg;
+      this.IsRelated = isRelated;
     }
 
     public string Filename { get; private set; }
@@ -24,11 +30,13 @@ namespace Microsoft.Research.Vcc
     public int Column { get; private set; }
     public int ErrorNumber { get; private set; }
     public string Message { get; private set; }
+    public bool IsRelated { get; private set; }
 
     public bool IsMatch(ErrorDetails other, bool compareMessage, bool compareLine, bool compareColumn)
     {
       return this.IsWarning == other.IsWarning
           && this.ErrorNumber == other.ErrorNumber
+          && this.IsRelated == other.IsRelated
           && (!compareLine || this.Line == other.Line)
           && (!compareColumn || this.Column == other.Column)
           && (!compareMessage || this.Message == other.Message);
@@ -36,23 +44,38 @@ namespace Microsoft.Research.Vcc
 
     public override string ToString()
     {
-      return string.Format("{0}({1},{2}): {3} ##{4:0000}: {5}", this.Filename, this.Line, this.Column,
-                           this.IsWarning ? "warning" : "error", this.ErrorNumber, this.Message);
+      return this.IsRelated ?
+        string.Format("{0}({1},{2}): {3} (Location of symbol related to previous {3}.)",
+                      this.Filename, this.Line, this.Column, this.IsWarning ? "warning" : "error") :
+        string.Format("{0}({1},{2}): {3} ##{4:0000}: {5}", this.Filename, this.Line, this.Column,
+                      this.IsWarning ? "warning" : "error", this.ErrorNumber, this.Message);
     }
 
     public static ErrorDetails Parse(string str)
     {
       var match = ErrorPattern.Match(str);
-      if (!match.Success) return null;
+      if (match.Success)
+      {
+        var filename = match.Groups["path"].Value;
+        var line = int.Parse(match.Groups["line"].Value);
+        var col = int.Parse(match.Groups["column"].Value);
+        var isWarning = match.Groups["kind"].Value == "warning";
+        var errno = int.Parse(match.Groups["errno"].Value);
+        var msg = match.Groups["msg"].Value;
+        return new ErrorDetails(filename, isWarning, line, col, errno, msg, false);
+      }
 
-      var filename = match.Groups["path"].Value;
-      var line = int.Parse(match.Groups["line"].Value);
-      var col = int.Parse(match.Groups["column"].Value);
-      var isWarning = match.Groups["kind"].Value == "warning";
-      var errno = int.Parse(match.Groups["errno"].Value);
-      var msg = match.Groups["msg"].Value;
+      match = RelatedErrorPattern.Match(str);
+      if (match.Success)
+      {
+        var filename = match.Groups["path"].Value;
+        var line = int.Parse(match.Groups["line"].Value);
+        var col = int.Parse(match.Groups["column"].Value);
+        var isWarning = match.Groups["kind"].Value == "warning";
+        return new ErrorDetails(filename, isWarning, line, col, 0, "", true);
+      }
 
-      return new ErrorDetails(filename, isWarning, line, col, errno, msg);
+      return null;
     }
   }
 }
--- vcc/CppDriver/TransEnv.cs
+++ vcc/CppDriver/TransEnv.cs
@@ -35,10 +35,13 @@ namespace Microsoft.Research.Vcc.Cpp
             EventHandler<ErrorReportedEventArgs> temp = ErrorReportedEvent;
             if (temp != null)
             {
-                temp(this, new ErrorReportedEventArgs(new ErrorDetails(tok.Filename, false, tok.Line, tok.Column, code, msg)));
+                temp(this, new ErrorReportedEventArgs(new ErrorDetails(tok.Filename, false, tok.Line, tok.Column, code, msg, false)));
             }
 
             Utils.Log(String.Format("{0}({1},{2}): error VC{3:0000}: {4}", tok.Filename, tok.Line, tok.Column, code, msg));
+
+            this.ReportRelated(false, related);
+
         }
 
         public override void Oops(Token tok, string msg)
@@ -48,7 +51,7 @@ namespace Microsoft.Research.Vcc.Cpp
                 EventHandler<ErrorReportedEventArgs> temp = ErrorReportedEvent;
                 if (temp != null)
                 {
-                    temp(this, new ErrorReportedEventArgs(new ErrorDetails(tok.Filename, false, tok.Line, tok.Column, -1, msg)));
+                    temp(this, new ErrorReportedEventArgs(new ErrorDetails(tok.Filename, false, tok.Line, tok.Column, -1, msg, false)));
                 }
 
                 Utils.Log(String.Format("{0}({1},{2}): oops: {3}", tok.Filename, tok.Line, tok.Column, msg));
@@ -60,10 +63,27 @@ namespace Microsoft.Research.Vcc.Cpp
             EventHandler<ErrorReportedEventArgs> temp = ErrorReportedEvent;
             if (temp != null)
             {
-                temp(this, new ErrorReportedEventArgs(new ErrorDetails(tok.Filename, true, tok.Line, tok.Column, code, msg)));
+                temp(this, new ErrorReportedEventArgs(new ErrorDetails(tok.Filename, true, tok.Line, tok.Column, code, msg, false)));
             }
 
             Utils.Log(String.Format("{0}({1},{2}): warning VC{3:0000}: {4}", tok.Filename, tok.Line, tok.Column, code, msg));
+
+            this.ReportRelated(true, related);
+
+        }
+
+        private void ReportRelated(bool isWarning, FSharp.Core.FSharpOption<Token> related)
+        {
+          if (FSharp.Core.FSharpOption<Token>.get_IsNone(related)) return;
+
+          var tok = related.Value;           
+          EventHandler<ErrorReportedEventArgs> temp = ErrorReportedEvent;
+          if (temp != null)
+          {
+                temp(this, new ErrorReportedEventArgs(new ErrorDetails(tok.Filename, isWarning, tok.Line, tok.Column, 0, "", true)));          
+          }
+          
+          Utils.Log(String.Format("{0}({1},{2}): {3} : (Location of symbol related to previous {3}.)", tok.Filename, tok.Line, tok.Column, isWarning ? "warning" : "error"));
         }
 
         public void Error(IToken tok, string msg)
@@ -72,7 +92,7 @@ namespace Microsoft.Research.Vcc.Cpp
             EventHandler<ErrorReportedEventArgs> temp = ErrorReportedEvent;
             if (temp != null)
             {
-                temp(this, new ErrorReportedEventArgs(new ErrorDetails(tok.filename, false, tok.line, tok.col, -2, msg)));
+                temp(this, new ErrorReportedEventArgs(new ErrorDetails(tok.filename, false, tok.line, tok.col, -2, msg, false)));
             }
 
             Utils.Log(String.Format("{0}({1},{2}): Boogie error: {3}", tok.filename, tok.line, tok.col, msg));
