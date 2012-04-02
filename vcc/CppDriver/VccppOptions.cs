1b7476f9a1c4c5944a8337271e5ca5a7428a352f
renehu <renehu@renehu2.europe.corp.microsoft.com>
2012-04-02 15:34:09 +0200

    Added support for spec pointers.

---
+++ vcc/CppDriver/VccppOptions.cs
@@ -0,0 +1,197 @@
+﻿namespace Microsoft.Research.Vcc.Cpp
+{
+    using Microsoft.Boogie;
+    using System.Collections.Generic;
+
+    class VccppOptions : CommandLineOptions
+    {
+        public VccppOptions()
+            : base("Vccpp", "VCC")
+        {
+        }
+
+        protected override bool ParseOption(string name, CommandLineOptionEngine.CommandLineParseState ps)
+        {
+            switch (name)
+            {
+                // Example
+                //case "rprint":
+                //    if (ps.ConfirmArgumentCount(1))
+                //    {
+                // = ps.args[ps.i];
+                //}
+                //break;
+
+                //TODO: set vccOptions
+                case "a":
+                case "aggressivepruning":
+                    return true;
+                case "b":
+                case "boogie":
+                    return true;
+                case "bvd":
+                    return true;
+                case "det":
+                case "determinize":
+                    return true;
+                case "d0":
+                case "dumpsource0":
+                    return true;
+                case "d":
+                case "dumpsource":
+                    return true;
+                case "f":
+                case "functions":
+                    return true;
+                case "F":
+                case "Functions":
+                    return true;
+                case "?":
+                case "help":
+                    return true;
+                case "h":
+                case "hide":
+                    return true;
+                case "i":
+                case "inspector":
+                    return true;
+                case "ii":
+                case "ignoreincludes":
+                    return true;
+                case "n":
+                case "nopreprocessor":
+                    return true;
+                case "nv":
+                case "noverification":
+                    return true;
+                case "oaf":
+                case "opsasfuncs":
+                    return true;
+                case "o":
+                case "out":
+                    return true;
+                case "p":
+                case "preprocessor":
+                    return true;
+                case "prelude":
+                    return true;
+                case "ps":
+                case "pointersize":
+                    return true;
+                case "pipe":
+                    return true;
+                case "weight":
+                    return true;
+                case "sm":
+                case "smoke":
+                    return true;
+                case "st":
+                case "stats":
+                    return true;
+                case "s":
+                case "suite":
+                    return true;
+                case "time":
+                    return true;
+                case "t":
+                case "translate":
+                    return true;
+                case "term":
+                case "termination":
+                    return true;
+                case "dexp":
+                case "defexpansion":
+                    return true;
+                case "version":
+                    return true;
+                case "wx":
+                case "warningsaserrors":
+                    return true;
+                case "w":
+                case "warn":
+                    return true;
+                case "xml":
+                    return true;
+                case "z":
+                case "z3":
+                    return true;
+                case "dt":
+                case "dumptriggers":
+                    return true;
+                default:
+                    break;
+            }
+
+            return base.ParseOption(name, ps);
+        }
+
+        protected override void ApplyDefaultOptions()
+        {
+            base.ApplyDefaultOptions();
+        }
+
+        public override void AttributeUsage()
+        {
+            // Provide attribute help here
+        }
+
+        public override void Usage()
+        {
+            Utils.Log(@"  ---- Vccpp options ---------------------------------------------------------
+/aggressivepruning             Remove unreferenced items when verifying specific functions (Short form: /a)
+/boogie:<boogie option list>   Options to pass on to Boogie (Short form: /b)
+/bvd                           Generate counter examples for the Boogie Verification Debugger
+/determinize                   Sort items to generate more deterministic Boogie files (Short form: /det)
+/dumpsource0                   Print source before syntax transformations (Short form: /d0)
+/dumpsource                    Print source after syntax transformations (Short form: /d)
+/functions:<function list>     List of function roots to verify (Short form: /f)
+/Functions:<function list>     List of exact function names to verify (Short form: /F)
+/help                          Display this usage message (Short form: /?)
+/hide:<warning list>           Hide specified warnings (by number; short form: /h)
+/inspector                     Run Z3 Inspector to show progress of verification (Short form: /i)
+/ignoreincludes[:<filename>]   Only verify entities from the files on the command line, not included files (Short form: /ii)
+                 With filename: verify only entities from that file
+/nopreprocessor                Do not invoke the C preprocessor on the input files (Short form: /n)
+/noverification                Do not run the verification; stop after semantic analysis (Short form: /nv)
+/opsasfuncs                    Translate arithmetic operations into Boogie functions (Short form: /oaf)
+/out:<directory>               Place generated files in <directory> (Short form: /o)
+/preprocessor:<pp option list> Options to pass on to the C preprocessor (Short form: /p)
+/prelude:<BPL prelude path>    Specify the path to an alternative VCC BPL prelude
+/pointersize:<32|64>           Size of the target platform's pointer type in bits (32,64), default is 64 (Short form: /ps)
+/pipe:<opt1>,...               Alter compiler pipeline. Can use . or + instead of the space in option names.
+                                     'active' lists stages
+                                     'dump after <stage>' dumps source after the stage
+                                     'dump after ...', ditto
+                                     'dump-all', dump after every stage (lots of output!)
+                                     'show-types', show expression types when dumping
+                                     'time', dump time takes by each stage
+                                     'disable <stage>' turns off a stage
+                                     'move <stage> before <stage>' moves stage in the pipeline
+                                     'move ... after ...', ditto
+                                     'isabelle <theory-name> <filename-prefix>', dump Isabelle theory
+/weight:<name=N>,...           Set weight for a particular kind of quantifier. Quantifiers with lower weights are instantiated first. 
+                                     To learn abouts kinds of quantifiers use /weight:dump-names /t and inspect the resulting .bpl file.
+                                     Specyfing /weight:foo=5 will set weight to 5 to 'foo' as well as 'foobar'.
+                                     The default weight for user supplied quantifier (user-exists and user-forall) is 10.
+                                     Most of the VCC generated ones have default of 1.
+                                     Definition axioms for lambda expressions (c-lambda-def) and _(def ...) function (c-def-function) have a default of 10.
+                                     You should avoid setting weight to 0.
+/smoke                         Run smoke tests, which is equivalent to /b:/smoke (Short form: /sm)
+/stats                         Display time statistics (Short form: /st)
+/suite <file or directory>     Treat the given file or directory as a collection of test cases with expected output and report any differences (Short form: /s)
+/time                          Display more detailed time statistics.
+/translate                     Translate the source files into corresponding BPL files (Short form: /t)
+/termination:<n>               Check termination behavior of: 0-nothing, 1-pure functions (default), 2-also ghost functions, 3-also regular functions (Short form: /term)
+/defexpansion:<n>              Specify number of expansion levels for recursive _(def ...) functions (default: 3; 0 is no limit; Short form: /dexp)
+/version                       Display the compiler version.
+/warningsaserrors              Treat warnings as errors (Short form: /wx)
+/warn:<n>                      Set warning level (0-2), default is 1 (Short form: /w)
+/xml:<fil>                     Write XML log to <file>.
+/z3:<z3 option list>           Options to pass on to Z3 (Short form: /z)
+/dumptriggers:<n>              1-report inferred triggers, 2-report results for different {:level}s, 3-report also when explicit triggers are present, 4-include Boogie form, 5-debug (Short form: /dt)
+");
+
+            base.Usage();  // Prints the Boogie options
+        }
+    }
+}
---
+++ vcc/CppDriver/VerificationErrorReporter.cs
@@ -0,0 +1,222 @@
+﻿using System;
+using Microsoft.Boogie;
+
+namespace Microsoft.Research.Vcc.Cpp
+{
+  class VerificationErrorReporter : VerifierCallback
+  {
+    private string currentFunction = "<unknown>";
+    private bool errorReported;
+
+    public event EventHandler<ErrorReportedEventArgs> ErrorReported;
+    public event EventHandler<VerificationFinishedEventArgs> VerificationFinished;
+    
+    public bool AnyErrorReported { get; private set; }
+
+    public override void OnCounterexample(Counterexample ce, string reason)
+    {
+      if (!errorReported)
+      {
+        OnVerificationFinished(currentFunction, "failed");
+        Utils.Log(String.Format("Verification of {0} failed.", this.currentFunction));
+      }
+
+      this.ReportCounterexample(ce, reason);
+      this.AnyErrorReported = true;
+      this.errorReported = true;
+    }
+
+    public override void OnOutOfMemory(string reason)
+    {
+      OnVerificationFinished(currentFunction, "ran out of memory");
+      Utils.Log(String.Format("Verification of {0} ran out of memory: {1}", this.currentFunction, reason));
+      this.AnyErrorReported = true;
+      this.errorReported = true;
+    }
+
+    public override void OnTimeout(string reason)
+    {
+      OnVerificationFinished(currentFunction, "timed out");
+      Utils.Log(String.Format("Verification of {0} timed out : {1}", this.currentFunction, reason));
+      this.AnyErrorReported = true;
+      this.errorReported = true;
+    }
+
+    public override void OnUnreachableCode(Implementation impl)
+    {
+      OnVerificationFinished(currentFunction, "smoke");
+
+      Utils.Log(String.Format("Verification of {0} found unreachable code", this.currentFunction));
+      this.AnyErrorReported = true;
+      this.errorReported = true;
+    }
+
+    public override void OnWarning(string msg)
+    {
+      OnVerificationFinished(currentFunction, "warning");
+      Utils.Log(String.Format("Verification of {0} gave warning: {1}", this.currentFunction, msg));
+    }
+
+    public void StartFunction(string functionName)
+    {
+      this.errorReported = false;
+      this.currentFunction = functionName;
+    }
+
+    public bool EndFunction()
+    {
+      if (!this.errorReported) {
+        OnVerificationFinished(currentFunction, "succeeded");
+        Utils.Log(String.Format("Verification of {0} succeeded.", this.currentFunction));
+        return true;
+      }
+
+      this.errorReported = false;
+      return false;
+    }
+
+    private void OnVerificationFinished(string name, string outcome)
+    {
+      EventHandler<VerificationFinishedEventArgs> temp = VerificationFinished;
+      if (temp != null)
+      {
+        temp(this, new VerificationFinishedEventArgs(name, outcome));
+      }
+    }
+
+    private void ReportError(IToken tok, int errno, string fmt, params object[] args)
+    {
+      var msg = String.Format(fmt, args);
+      
+      EventHandler<ErrorReportedEventArgs> temp = ErrorReported;
+      if (temp != null)
+      {
+        temp(this, new ErrorReportedEventArgs(new ErrorDetails(tok.filename, false, tok.line, tok.col, errno, msg)));
+      }
+
+      Utils.Log(String.Format("{0}({1},{2}): error VC{3:0000}: {4}", tok.filename, tok.line, tok.col, errno, String.Format(fmt, args)));
+    }
+
+
+    private void ReportCounterexample(Counterexample ce, string message)
+    {
+      if (message != null) message = " (" + message + ")";
+      else message = "";
+
+      var rce = ce as ReturnCounterexample;
+      if (rce != null)
+      {
+        IToken tok = rce.FailingReturn.tok;
+        for (int i = rce.Trace.Length - 1; i >= 0; i--)
+        {
+          foreach (Cmd c in rce.Trace[i].Cmds)
+          {
+            var assrt = c as AssertCmd;
+            if (assrt != null)
+            {
+              var nary = assrt.Expr as NAryExpr;
+              if (nary != null)
+              {
+                var fcall = nary.Fun as FunctionCall;
+                if (fcall != null && fcall.FunctionName == "$position_marker")
+                {
+                  tok = assrt.tok;
+                }
+              }
+            }
+          }
+        }
+
+        ReportOutcomePostconditionFailed(rce.FailingEnsures.tok, tok, message);
+        return;
+      }
+
+      var ace = ce as AssertCounterexample;
+      if (ace != null)
+      {
+        ReportOutcomeAssertFailed(ace.FailingAssert.tok,
+                                  (ace.FailingAssert is LoopInvMaintainedAssertCmd
+                                     ? "Loop body invariant"
+                                     : ace.FailingAssert is LoopInitAssertCmd ? "Loop entry invariant" : "Assertion"),
+                                  message);
+        return;
+      }
+
+      var cce = ce as CallCounterexample;
+      if (cce != null)
+      {
+        ReportOutcomePreconditionFailed(cce.FailingCall.tok, cce.FailingRequires, message);
+        return;
+      }
+
+      System.Diagnostics.Debug.Assert(false, "Unexpected counter example type");
+    }
+
+    private void ReportOutcomeAssertFailed(IToken assertTok, string kind, string comment)
+    {
+      var errnoAndMsg = GetErrorNumber(assertTok.val, 9500);
+      ReportError(assertTok, errnoAndMsg.Item1, "{0}{2} '{1}' did not verify.", kind, errnoAndMsg.Item2, comment);
+      ReportAllRelated(assertTok);
+    }
+
+    private void ReportOutcomePreconditionFailed(IToken callTok, Requires req, string addComment)
+    {
+      string /*?*/ comment = req.Comment;
+      IToken reqTok = req.tok;
+      if (comment != null) comment = ": " + comment;
+      else comment = "";
+      comment += addComment;
+
+      string reqMsg = reqTok.val;
+
+      ReportError(callTok, 9502, "Call '{0}' did not verify{1}.", RemoveWhiteSpace(callTok.val), comment);
+      ReportRelated(reqTok, "Precondition: '{0}'.", reqMsg);
+      ReportAllRelated(reqTok);      
+    }
+
+    private void ReportOutcomePostconditionFailed(IToken ensTok, IToken retTok, string comment)
+    {
+      string msg = ensTok.val;
+      if (retTok.line == 0) retTok = ensTok;
+      if (ensTok.line == 0) ensTok = retTok;
+
+      ReportError(retTok, 9501, "Post condition{0} '{1}' did not verify.", comment, msg);
+      ReportRelated(ensTok, "Location of post condition.");
+      ReportAllRelated(ensTok);
+    }
+
+    private void ReportAllRelated(IToken tok)
+    {
+      var btok = tok as BoogieToken.Token;
+
+      while (btok != null && btok.Related != null) {
+        ReportRelated(btok.Related.Value, ((IToken)btok.Related.Value).val + ".");
+        btok = btok.Related.Value;
+      }
+    }
+
+    private void ReportRelated(IToken tok, string fmt, params string[] args)
+    {
+      ReportError(tok, 9599, "(related information) " + fmt, args);
+    }
+
+    public static string RemoveWhiteSpace(string str)
+    {
+      return String.Join(" ", Array.FindAll(str.Split(new[] { '\n', '\r', '\t', ' ' }), s => !String.IsNullOrEmpty(s)));
+    }
+
+    private static Tuple<int, string> GetErrorNumber(string msg, int _default)
+    {
+      msg = RemoveWhiteSpace(msg);
+      if (msg.StartsWith("#VCCERR:")) {
+        int i = 8;
+        while (i < msg.Length && char.IsDigit(msg[i])) i++;
+        int num = int.Parse(msg.Substring(8, i - 8));
+        msg = msg.Substring(i + 1);
+        return Tuple.Create(num, msg);
+      } 
+        
+      return Tuple.Create(_default, msg);
+    }
+  }
+}
