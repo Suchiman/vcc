b533fcf0d23e838fdb08d71e6bd418d517b330ac
renehu <renehu@renehu2.europe.corp.microsoft.com>
2012-05-21 16:48:18 +0200

    Added support for skip_smoke.

---
+++ vcc/VSPackage/NativeMethods.cs
@@ -0,0 +1,11 @@
+﻿namespace Microsoft.Research.Vcc.VSPackage
+{
+    using System;
+    using System.Runtime.InteropServices;
+
+    internal class NativeMethods
+    {
+        [DllImport("user32.dll")]
+        static internal extern IntPtr GetForegroundWindow(); 
+    }
+}
