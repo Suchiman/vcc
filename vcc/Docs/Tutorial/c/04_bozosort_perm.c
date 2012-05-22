709033ff75131d5810389cd9467ca641b14ae6a8
renehu <renehu@renehu2.europe.corp.microsoft.com>
2012-05-22 11:58:18 +0200

    Added support for claims_object.

---
+++ vcc/Docs/Tutorial/c/04_bozosort_perm.c
@@ -0,0 +1,59 @@
+#include "02_rand.c"
+
+/*{begin}*/
+_(logic bool sorted(int *buf, unsigned len) =
+  \forall unsigned i, j; i < j && j < len ==> buf[i] <= buf[j])
+
+_(typedef unsigned perm_t[unsigned]; )
+
+_(logic bool is_permutation(perm_t perm, unsigned len) =
+  (\forall unsigned i, j;
+    i < j && j < len ==> perm[i] != perm[j]))
+
+_(logic bool is_permuted(\state s, int *buf, unsigned len,
+                         perm_t perm) =
+  \forall unsigned i; i < len ==>
+      perm[i] < len && \at(s, buf[ perm[i] ]) == buf[i])
+
+_(logic perm_t swap(perm_t p, unsigned i, unsigned j) =
+      \lambda unsigned k; k == i ? p[j] : k == j ? p[i] : p[k])
+
+void bozo_sort(int *buf, unsigned len _(out perm_t perm))
+  _(writes \array_range(buf, len))
+  _(ensures sorted(buf, len))
+  _(ensures is_permutation(perm, len))
+  _(ensures is_permuted(\old(\now()), buf, len, perm))
+{
+  _(ghost \state s0 = \now() )
+
+  _(ghost perm = \lambda unsigned i; i)
+
+  if (len == 0) return;
+
+  for (;;)
+    _(invariant \mutable_array(buf, len))
+    _(invariant is_permutation(perm, len))
+    _(invariant is_permuted(s0, buf, len, perm))
+  {
+    int tmp;
+    unsigned i = random(len), j = random(len);
+
+    tmp = buf[i];
+    buf[i] = buf[j];
+    buf[j] = tmp;
+    _(ghost perm = swap(perm, i, j) )
+
+    for (i = 0; i < len - 1; ++i)
+      _(invariant sorted(buf, i + 1))
+    {
+      if (buf[i] > buf[i + 1]) break;
+    }
+
+    if (i == len - 1) break;
+  }
+}
+/*{out}*/
+/*`
+Verification of random succeeded.
+Verification of bozo_sort succeeded.
+`*/
---
+++ vcc/SyntaxHighlightingParser/Ast.fs
@@ -0,0 +1,211 @@
+﻿namespace SyntaxHighlighting
+
+open Microsoft.FSharp.Text.Lexing
+
+module Ast =
+
+  type Span = 
+    | Spec of int * int
+    | Keyword of int * int
+
+  let private toKeywordSet =  
+    List.map (fun (str:string) -> str.ToCharArray()) >> Set.ofList
+
+  let private keywords =
+    toKeywordSet [
+                  "abstract" ;
+                  "admissibility" ;
+                  "always" ;
+                  "atomic" ;
+                  "atomic_inline" ;
+                  "atomic_op";
+                  "atomic_read" ;
+                  "as_array" ;
+                  "assume" ;
+                  "assume_correct" ;
+                  "assert" ; 
+                  "axiom" ; 
+                  "backing_member" ;
+                  "begin_update" ;
+                  "blob" ;
+                  "blobify" ;
+                  "blob_of" ;
+                  "bump_volatile_version" ;
+                  "by_claim" ;
+                  "claimable" ;
+                  "colon" ;
+                  "contract" ;
+                  "contract_pointer" ;
+                  "CommandLineSwitches" ;
+                  "datatype" ; 
+                  "decreases" ; 
+                  "def" ;
+                  "dynamic_owns" ;
+                  "ensures" ; 
+                  "frame_axiom" ;
+                  "ghost" ;
+                  "ghost_atomic" ;
+                  "group" ;
+                  "havoc_others" ;
+                  "increases" ;
+                  "inline" ;
+                  "invariant" ;
+                  "isolate_proof" ;
+                  "join_blobs" ;
+                  "known" ;
+                  "maintains" ;
+                  "no_reads_check" ;
+                  "option" ;
+                  "out" ;
+                  "precise" ;
+                  "primitive" ;
+                  "pure" ;
+                  "read_only" ;
+                  "reads" ;
+                  "reads_havoc" ;
+                  "record" ;
+                  "requires" ;
+                  "recursive_with";
+                  "returns" ;
+                  "retype" ;
+                  "root_array" ;
+                  "root_index" ;
+                  "skip_smoke" ;
+                  "split_blob" ;
+                  "TestResults" ;
+                  "type" ;
+                  "typedef" ;
+                  "unblobify" ;
+                  "unchecked" ;
+                  "union_reinterpret" ;
+                  "unwrap" ;
+                  "unwrapping" ;
+                  "updates" ;
+                  "volatile_owns" ;
+                  "wrap" ;
+                  "writes" ;
+                  "yarra" ;
+    ]
+
+  let private guardedKeywords =
+    toKeywordSet [
+                  "\\account_claim" ;
+                  "\\active_claim" ;
+                  "\\addr" ;
+                  "\\addr_eq" ;
+                  "\\alloc" ;
+                  "\\alloc_array" ;
+                  "\\always_by_claim" ;
+                  "\\approves"; 
+                  "\\array_members" ;
+                  "\\array_range" ;
+                  "\\arrays_disjoint" ;
+                  "\\at" ;
+                  "\\begin_update" ;
+                  "\\claim" ;
+                  "\\claim_count" ;
+                  "\\claimable" ;
+                  "\\claims"; 
+                  "\\claims_claim" ;
+                  "\\claims_object" ;
+                  "\\closed" ;
+                  "\\composite_extent" ;
+                  "\\deep_eq" ;
+                  "\\deep_unwrap" ;
+                  "\\depends" ;
+                  "\\destroy_claim" ;
+                  "\\diff" ;
+                  "\\disjoint" ;
+                  "\\domain" ;
+                  "\\domain_root" ;
+                  "\\domain_updated_at" ;
+                  "\\embedding" ;
+                  "\\exists" ;
+                  "\\extent" ;
+                  "\\extent_fresh" ;
+                  "\\extent_mutable" ;
+                  "\\false" ;
+                  "\\forall" ;
+                  "\\fresh" ;
+                  "\\from_bytes" ;
+                  "\\full_context" ;
+                  "\\full_extent" ;
+                  "\\gemb" ;
+                  "\\in_range_phys_ptr";
+                  "\\in_range_spec_ptr";
+                  "\\inter" ;
+                  "\\in" ;
+                  "\\in0" ;
+                  "\\in_array";
+                  "\\index_within";
+                  "\\integer" ;
+                  "\\inv" ;
+                  "\\inv2" ;
+                  "\\inv2s" ;
+                  "\\is" ;
+                  "\\is_array" ;
+                  "\\join_arrays" ;
+                  "\\lambda";
+                  "\\make_claim" ;
+                  "\\malloc_root" ;
+                  "\\map" ;
+                  "\\match_long" ;
+                  "\\match_ulong" ;
+                  "\\may_diverge" ;
+                  "\\mine";
+                  "\\mutable" ;
+                  "\\mutable_array" ;
+                  "\\natural" ;
+                  "\\nested" ;
+                  "\\non_primitive_ptr" ;
+                  "\\not_shared" ;
+                  "\\now" ;
+                  "\\object" ;
+                  "\\object_root" ;
+                  "\\objset" ;
+                  "\\old" ;
+                  "\\on_unwrap" ;
+                  "\\owner" ;
+                  "\\owns" ;
+                  "\\program_entry_point" ;
+                  "\\recursive_with";
+                  "\\result";
+                  "\\set" ;
+                  "\\set_closed_owns" ;
+                  "\\shallow_eq" ;
+                  "\\simple_embedding" ;
+                  "\\size" ;
+                  "\\size_t" ;
+                  "\\span" ;
+                  "\\split_array" ;
+                  "\\start_here";
+                  "\\state" ;
+                  "\\subset" ;
+                  "\\this" ;
+                  "\\thread" ;
+                  "\\thread_local" ;
+                  "\\thread_local_array" ;
+                  "\\to_bytes" ;
+                  "\\true" ;
+                  "\\type" ;
+                  "\\typeof" ;
+                  "\\unchanged";
+                  "\\union" ;
+                  "\\union_active" ;
+                  "\\union_reinterpret" ;
+                  "\\universe" ;
+                  "\\unwrap" ;
+                  "\\upgrade_claim" ;
+                  "\\valid" ;
+                  "\\vdomain" ;
+                  "\\when_claimed"; 
+                  "\\wrap" ;
+                  "\\wrapped" ;
+                  "\\wrapped0";
+                  "\\wrapped_with_deep_domain" ;
+                  "\\writable" ;
+    ]
+
+  let isGuardedKeyword chars = Set.contains chars guardedKeywords
+
+  let isKeyword chars = Set.contains chars keywords
