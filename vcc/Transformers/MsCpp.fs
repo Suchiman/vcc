6cce65711f5502bc0e4854cfb681680cf31c4ece
dseifert <dseifert@localhost>
2012-05-23 08:27:09 +0200

    fixed merge problem

--- vcc/Transformers/MsCpp.fs
+++ vcc/Transformers/MsCpp.fs
@@ -331,12 +331,12 @@ namespace Microsoft.Research.Vcc
         | Call (ec, { FriendlyName = "VCC::Integer::operator int" }, [], [AddrOf(_, arg)])
         | Call (ec, { FriendlyName = "VCC::Integer::operator int" }, [], [arg]) ->
           Some(self arg)
-        | AddrOf(_, Deref(_, Call (ec, { FriendlyName = StartsWith "VCC::Integer::Integer" }, [], [arg0; arg1])))
-        | Deref(_, Call (ec, { FriendlyName = StartsWith "VCC::Integer::Integer" }, [], [arg0; arg1]))
-            | Call (ec, { FriendlyName = "VCC::Integer::operator-=" }, [], [arg0; arg1]) ->
+        | Call (ec, { FriendlyName = "VCC::Integer::operator-=" }, [], [arg0; arg1]) ->
           Some(Prim(ec, Op("-=", CheckedStatus.Checked), [self arg0; self arg1]))
         | Call (ec, { FriendlyName = "VCC::Integer::operator+=" }, [], [arg0; arg1]) ->
           Some(Prim(ec, Op("+=", CheckedStatus.Checked), [self arg0; self arg1]))
+        | AddrOf(_, Deref(_, Call (ec, { FriendlyName = StartsWith "VCC::Integer::Integer" }, [], [arg0; arg1])))
+        | Deref(_, Call (ec, { FriendlyName = StartsWith "VCC::Integer::Integer" }, [], [arg0; arg1]))
         | Call (ec, { FriendlyName = StartsWith "VCC::Integer::Integer" }, [], [arg0; arg1]) ->
           Some(Cast({ ec with Type = Type.MathInteger MathIntKind.Signed }, CheckedStatus.Checked, self arg1))
         | _ -> None
